4bd9eafc04363eaa3489abedb841ddd4c640960e
Mark <teamofmark@gmail.com>
2020-02-19 11:21:16 +0900

    연산자 실습예제 배포

--- JS/step1/js/1_4_operator.js
+++ JS/step1/js/1_4_operator.js
@@ -142,3 +142,28 @@ function logicalOperator1(){
     var result = (a=="여자")&&(b=="웹디자이너");
     console.log("result = " + result); 
 }
+// * case.9 a변수에는 '남자', b변수에는 '웹퍼블리셔' 라는 값이 저장되어 있다.
+// * 담겨있는 두 값중 하나라도 맞는지 result 변수에 저장후 출력.
+
+function logicalOperator2(){
+    var a = "여자";
+    var b = "웹퍼블리셔";
+    var result = (a=="남자")||(b=="웹퍼블리셔");
+    console.log("result = " + result);
+}
+
+// todo.6 다음 코드를 작성 후 결과를 예상한 다음 출력하라.
+
+function compareOperator4(){
+    console.log(30 > 20 > 10); // todo. A : ?
+}
+
+// todo.6-1 5번예제를 정상적인 결과가 출력되도록 수정.
+function compareOperator4R(){
+    console.log(30 > 20 && 20 > 10);
+}
+/*
+    ! (30 > 20 > 10); 의 결과식은 거짓으로 판별한다. 
+    ? (30>20)>10  , true > 10 , 1>10  , false
+    그래서 논리곱 연산자를 사용하여 좌측과 우측을 따로 계산후 결과 값을 산출
+*/

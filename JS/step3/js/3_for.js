d67d0bfcdfb9e99687e9c39ca175a97cd4f602db
Mark <teamofmark@gmail.com>
2020-02-25 12:39:09 +0900

    피라미드 실습예제 배포

--- JS/step3/js/3_for.js
+++ JS/step3/js/3_for.js
@@ -77,3 +77,151 @@ function answer5(){
         document.write((11-i) + "<br>");//? 1~10번 까지 출력
     }
 }
+
+// todo.4 * 10개 찍힌 결과 출력.
+
+function star10(){
+    var star = "";
+    for (var i = 0; i < 10; i++){
+        star += "*";
+    }
+    document.write("result =" + star);
+}
+
+// *exp.3 자신이 좋아하는 과일 4개를 배열로 배치하고, alert으로 출력.
+
+function favorFluit(){
+    var array = ['포도', '오렌지', '바나나', '딸기'];
+    
+    for (var i = 0; i < array.length; i++){
+        alert(array[i]);
+    }
+}
+
+// *exp.4 지금까지 배웠던 JS를 배열을 활용해 출력해보자.
+ function arrayFor(){
+     var data = ["변수", "연산자", "형변환", "조건문 if", "조건문 switch", "반복문 for", "반복문 while"];
+    /*
+        ?변수 연산자 형변환 조건문if 조건문 switch 반복문 for 반복문 while
+    */
+    for (var i = 0; i < data.length; i++){
+        document.write((i + 1) + "번째 내용 = " + data[i] + '<br>');
+    }
+}
+
+function arrayForES6(){
+    var data = ["변수", "연산자", "형변환", "조건문 if", "조건문 switch", "반복문 for", "반복문 while"];
+
+    for (let i = 0; i < data.length; i++){
+        const element = data [i];
+        document.write((i + 1) + "번째 내용 = " + element + '<br>');
+    }
+}
+
+// *exp.6 역반복 배열
+function reverseFavorFluit(){
+    var array = ['포도', '오렌지', '바나나', '딸기'];
+
+    for (var i = array.length - 1; i >=0; i--){
+        //?i 변수는 배열의 갯수 -1 (4-1 = 3) 로 선언, 그 변수가 0보다 크거나 같을때 까지 (3>=0 ----> 3,2,1,0) 실행 .
+        alert(array[i]);//?그래서 3,2,1,0 순으로 출력. 이것을 역반복
+    }
+}
+
+// *exp.7 continue문
+function forContinue(){
+    for (var i = 0; i <= 10; i++){
+        continue; //?
+        document.write(i + "<br>"); //?
+    }
+    document.write("최종 i = " + i + "<br>"); //? 최종 변경된 i의 수치 11
+}
+
+// *exp.8 continue활용
+function runContinue(){
+    var output = 0;
+    for (var i = 1; i <= 10; i++){ //?
+
+        if(i % 2 == 1){
+            continue;
+            /*
+                ? 홀수필터
+                i가 홀수일 경우 반복 중지 다음반복수행, 현재 다음 반복은 없다.
+                (1 을 2로 나눈 나머지가 1과 일치 할경우  다음 반복을 수행하라.) 다시 반복문으로
+                (2 를 2로 나눈 나머지가 1과 일치 할경우  다음 반복을 수행하라.) 이탈후 output += i; 실행
+                (3 을 2로 나눈 나머지가 1과 일치 할경우  다음 반복을 수행하라.) 다시 반복문으로
+                (4 를 2로 나눈 나머지가 1과 일치 할경우  다음 반복을 수행하라.) 이탈후 output += i; 실행
+                ..
+                이조건으로 i값이 10보다 작거나 같을때까지 수행하여 출력하라.
+            */
+        }
+        output += i; //? (2, 6, 12, 20, 30) - 짝수만 선별하여 10까지의 셈합.
+
+        alert(output); //?
+    }
+}
+// * exp.9 break문
+function forBreak(){
+    for (var i = 1; i <= 10; i++){
+        break; //? 위 조건이 참일 지라도 break가 존재 하면, 반복루프중단.
+        document.write(i + "<br>"); //? 수행불가.
+    }
+    document.write("최종 i =" + i + "<br>"); //? 루프가 실행되지않아 1.
+}
+
+// *exp.10 break문 활용
+function runBreak(){
+    for (var i = 0; true; i++){ //? 수행되는 조건 자체가 true 상태기 때문에 무한 루프.
+        alert(i + "번째 반복문"); //? 수행. 
+        if(!confirm('계속할래?')){ //? alert으로 반복문을 띄운 이후, confirm의 질문에 확인(true)을 누르면 앞의 논리부정연산자로인해 true가 false로 바뀌면서 break를 수행하지 않고 다시 재반복을 수행한다.
+            break;
+        }
+    }
+}
+
+// ! 다중for
+
+function halfPyramid(){
+    var star = '';
+    for (var i = 1; i <= 10; i++){ //? 1. ifor = 1~10 까지 수행
+
+        for (var j = 0; j < i; j++){ //? 2. jfor = j가 i보다 작을때 까지 반복 수행 *,**,***,****,*****......
+            star += '*'; //? 변수 star에 생성된 *을 넣음.
+        }
+
+        star += '<br>';//? ifor 만큼 <br>을 변수 star에 넣음
+    }
+    document.write(star);//? 반절 피라미드 출력.
+}
+
+// *todo.1 역반절 피라미드
+function reverseHalfPyramid(){ 
+    var star = '';
+    
+    for (var i = 10; i >= 1; i--){//? 1. ifor = 10~1 까지 수행
+
+        for(var j = 0; j < i; j++){ //? jfor = j가 i보다 작거나 같을 때 까지 반복 수행 10,9,8,7,6....
+            star += '*'; //? 변수 star에 반복생성된 만큼 *을 넣음.
+        }
+
+        star += '<br>'; //? i for 만큼 <br>을 변수 star에 넣음.
+    }
+    document.write(star);
+
+}
+
+//*todo.2 피라미드
+function pyramid(){
+
+    for(){
+
+        for(){
+
+        }
+        for(){
+            
+        }
+
+    }
+
+}

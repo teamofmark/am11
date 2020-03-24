0ddd3a8856c52027b5131a1eee53912b4760cc9e
Mark <teamofmark@gmail.com>
2020-03-24 12:02:05 +0900

    배열클래스 진행중

--- JS/step4/js/7_arrayClass.js
+++ JS/step4/js/7_arrayClass.js
@@ -13,3 +13,56 @@
 */
 
 // * ex.1 배열 요소 개수 알아내기
+function arrCount(){
+    var users = ["user1", "user2", "user3", "user4"];
+    console. log(users.length);
+}
+
+// * ex.2 특정 위치의 배열 요소 접근하기.
+function targetArr(){
+    var menuItems = ["menu1", "menu2", "menu3", "menu4"];
+
+    for (var i = 0; i < menuItems.length; i++){
+        var element = menuItems[i];
+        console.log(i + " = " + element);
+    }
+}
+
+// *ex.3 배열을 문자열로 만들기.
+
+function joinArr(){
+    var items = ["item1", "item2", "item3", "item4"];
+    var result = items.join("-");
+    console.log(result);
+}
+
+/*
+    ! 배열.join("separator")
+    ! 매개변수 = separator, 선택사항. 배열 요소 구분용도의 문자열. 생략시 , 가 구분자.
+    ! 리턴값 = 지정된 구분자로 배열 요소들을 이어 붙혀서 만들어진 문자열
+*/
+
+// *ex.4 문자열을 배열로 만들기.
+var $menu = null;
+$(document).ready(function(){
+    init();
+    stringArr();
+});
+
+function init(){
+    $menu = $("nav ul");
+}
+function stringArr(){
+    var menuData = "menu1, menu2, menu3, menu4";
+    var menuItems = menuData.split(",");
+    for (var i = 0; i < menuItems.length; i++){
+        var result = menuItems[i];
+        var addMenuItem = "<li>" + result + "</li>";
+        $menu.append(addMenuItem);
+    }
+}
+/*
+    ! 배열.split(separator)
+    ! 매개변수 = separator, 구분자로 사용할 문자열.
+    ! 리턴값 = 구분자로 나눠 만들어진 배열.
+*/

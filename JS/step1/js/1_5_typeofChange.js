3b3c776d884217dedf50d7075e327976956f830c
Mark <teamofmark@gmail.com>
2020-07-15 11:39:01 +0900

    문자형을 숫자형으로 강제변환하여 연산가능하게

--- JS/step1/js/1_5_typeofChange.js
+++ JS/step1/js/1_5_typeofChange.js
@@ -96,20 +96,37 @@ todo.1 result에 대입하라.
 
 function todo1(){
     var test = 100;
-    var result = ?;
+    var result = test + '';
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 1-2 string 사용.
 
 function todo2(){
     var test = 100;
-    var result = ?;
+    var result = String(test);
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 1-3 toString 사용.
 
 function todo3(){
     var test = 100;
-    var result = ?;
+    var result = test.toString();
     console.log(typeof(result) + ' = ' + result);
+}
+// todo.2 문자 형을 숫자 형으로 변환하여 연산이 가능하게 만들어라.
+
+// todo.2-1 parse 사용
+
+function todo4(){
+    var test = "100.50";
+    var result = test + 150 + "20";
+    console.log("result = " + result);
+}
+
+// todo. 2-2 Number 사용
+
+function todo5(){
+    var test = "100.50";
+    var result = test + 150 + "20";
+    console.log("result = " + result);
 }

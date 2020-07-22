f5ecc9369b4a8e64447f7076771db2f1c855bb7d
Mark <teamofmark@gmail.com>
2020-07-22 10:43:49 +0900

    함수 진행중

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -25,3 +25,41 @@ function helloFunc(){//!함수로 호출시
     document.write(comment,"<br>");
 }
 //helloFunc();helloFunc();helloFunc();
+
+//* exp.2 기존의 반절피라미드 2회를 함수를 만들어 간결하게.
+/*
+var star = '';
+for (var i = 1; i <= 10; i++) { 
+    for (var j = 0; j < i; j++) { 
+        star += '*'; 
+    }
+    star += '<br>';
+}
+document.write(star);
+var star = '';
+for (var i = 1; i <= 10; i++) { 
+    for (var j = 0; j < i; j++) { 
+        star += '*'; 
+    }
+    star += '<br>';
+}
+document.write(star);
+
+function starPrint(){
+    var star = '';
+    for (var i = 1; i <= 10; i++) { 
+        for (var j = 0; j < i; j++) { 
+            star += '*'; 
+        }
+        star += '<br>';
+    }
+    document.write(star);
+}
+starPrint();starPrint();
+*/
+// *exp.3 매개변수(Parameter)가 있는 함수 만들기
+
+function sumParam(){
+    var result = num1 + num2;
+    alert('두 수의 합은 = ' + result);
+}

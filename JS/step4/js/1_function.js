f3623d8b8ea7ff4982108d3d826ea37d5527f6c3
Mark <teamofmark@gmail.com>
2020-07-23 10:50:55 +0900

    계산기 2차 분할 진행중

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -207,8 +207,30 @@ function calculator(op, numb1, numb2){
     }
     return result;
 }
-document.write("1 결과 = " + calculator("+",20,10) + '<br>');
-document.write("2 결과 = " + calculator("-",20,10) + '<br>');
-document.write("3 결과 = " + calculator("*",20,10) + '<br>');
-document.write("4 결과 = " + calculator("/",20,10) + '<br>');
-document.write("5 결과 = " + calculator("%",20,10) + '<br>');
+// document.write("1 결과 = " + calculator("+",20,10) + '<br>');
+// document.write("2 결과 = " + calculator("-",20,10) + '<br>');
+// document.write("3 결과 = " + calculator("*",20,10) + '<br>');
+// document.write("4 결과 = " + calculator("/",20,10) + '<br>');
+// document.write("5 결과 = " + calculator("%",20,10) + '<br>');
+
+function calculator2(op, numb1, numb2){
+    var result = '';
+    switch(op){
+        case "+":
+            result = add(numb1,numb2);
+            break;
+        case "-" :
+            result = sub(numb1,numb2);
+            break;
+        case "*" :
+            result = mul(numb1,numb2);
+            break;
+        case "/" :
+            result = div(numb1,numb2);
+            break;
+        default:
+            result = "잘못된 연산자 입니다.";
+            break;
+    }
+    return result;
+}

e2cd574cea4dfc50011002408f32a74ca8e4aa6c
Mark <teamofmark@gmail.com>
2020-07-31 11:05:35 +0900

    구조분리 완성

--- JQuery/js/etc5.js
+++ JQuery/js/etc5.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    
+    imgAlignCustom(5, 300, 200);
 });
 
-function imgAlignCustom(){
+function imgAlignCustom(count, imgWidth, imgHeight){
     var img = $(".imgContainerCustom img");
     var xpos = 0;
     var ypos = 0;
@@ -10,13 +10,13 @@ function imgAlignCustom(){
 
     $(".alignMix").click(function(){
         var imgLength = img.length;
-        var imgSize = img.width();
+        // var imgWidth = img.width();
         // var imgHeight = img.height();
 
         for (var i = 0; i < imgLength; i++){
             var image = img.eq(i);
-            xpos = (i%3)*imgSize; //? 0/3 = 나머지 0, 1/3 = 1, 2/3 = 2, 3/3 = 0 ....*imgSize
-            ypos = parseInt(i/3) *imgSize; //? 0/3 = 0 , 1/3 = 0, 2/3 = 0, 3/3 = 1, 3/4 = 1, 3/5 = 1... *imgHeight
+            xpos = (i%count)*imgWidth; //? 0/3 = 나머지 0, 1/3 = 1, 2/3 = 2, 3/3 = 0 ....*imgSize
+            ypos = parseInt(i/count) *imgHeight; //? 0/3 = 0 , 1/3 = 0, 2/3 = 0, 3/3 = 1, 3/4 = 1, 3/5 = 1... *imgHeight
 
             image.css({
                 "left" : xpos,

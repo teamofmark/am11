216818f928a6abfe30c65d931bff4d1da30bb599
Mark <teamofmark@gmail.com>
2020-12-02 11:55:25 +0900

    계산기 매개변수처리

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -54,3 +54,124 @@ function sumAll(){
 //     }
 //     return willReturn;
 // }
+
+// *exp.4 매개변수의 자리가 없는 곳에 입력된 매개변수 를 arguments를 이용하여 출력하게 만들어보자.
+
+function showUserInfo(){
+    console.log('이름 = ' + arguments[0]);
+    console.log('나이 = ' + arguments[1]);
+    alert(arguments[0]+ "의 나이는" + arguments[1] + "세 입니다.");
+}
+/*
+    ?return (리턴)
+    함수 내부는 함수라는 범위 안에 갇히기 때문에, 한번 실행되면 함수 외부에서 접근 할 수 없다.
+    이때 매개변수를 활용하면 함수 내부로 데이터를 전달 할 수 있는데, 리턴값은 이와 반대 되는 값.
+    즉, 매개변수 값이 함수 외부에서 내부로 들어오는 입력 값이라면, 리턴값은 함수 내부에서 처리한 결과값을
+    함수 외부로 전달하기 위해 사용 하는 출력값.
+
+
+    !ex.1 함수f(x)안에 넣는 값이 매개변수, 결과로 나오는 x*x 가 리턴값.
+    * function f(x) { return x * x; }
+    * alert(f(3));
+
+    !ex.2 alert에서 매개변수를 두개를 선언하면 추가된 매개변수를 무시한다.
+    //*(매개변수가 초과 됐을 시) 또한 원래 함수의 매개변수보다 적게 입력하면 undefined를 출력한다.
+        alert('기존매개변수', '추가매개변수');
+        prompt('기존매개변수');
+*/
+
+// *exp.5 두 수를 매개변수로 받고, 두 값을 더한 결과값을 리턴하는 함수를 만들어보자.
+
+function sumReturn1(num1, num2){
+    var result = num1 + num2;
+    return result;
+}
+var value = sumReturn1(13,26);
+
+// *exp.6 무한루프를 돌며 숫자를 입력받고 입력받은 수의 합을 화면에 출력하는 함수를 만들어보자.
+// ! 단 입력값이 0이면 즉시 실행을 멈추게 한다.
+
+function infiniteSum(){
+    var sum = 0;
+    var count = 1;
+
+    while(true){
+        var value = parseInt(window.prompt('숫자만 입력해라.'));
+        if(value == 0){
+            document.write('종료');
+            return;
+        }
+        sum += value;
+        document.write(count + '.' + sum + '<br>');
+        count ++;
+    }
+    document.write('총' + count + '번 실행함.');
+}
+
+function printGugudan(){
+    var startNumb = parseInt(window.prompt('최소단수'));
+    var lastNumb = parseInt(window.prompt('최대단수'));
+
+    for ( var i = startNumb; i <= lastNumb; i++){
+        document.write(i + '단 출력', '<br>');
+        for(var m = 1; m <= lastNumb; m++){
+            document.write(i + '*' + m + '=' + (i*m), '<br>');
+        }
+        document.write('<br>');
+    }
+}
+
+// todo.2 다음실행구문으로 전달받은 매개변수로 계산하여 결과를 출력하는 함수 만들기.
+/*
+    //* 실행구문
+    * document.write("1 결과 =" + calculator("+", 20, 10));
+    * document.write("2 결과 =" + calculator("-", 20, 10));
+    * document.write("3 결과 =" + calculator("*", 20, 10));
+    * document.write("4 결과 =" + calculator("/", 20, 10));
+    * document.write("5 결과 =" + calculator("%", 20, 10));
+    
+    //* 결과
+    * 결과 = 30
+    * 결과 = 10
+    * 결과 = 200
+    * 결과 = 2
+    * 결과 = 잘못된 연산자 입니다.
+*/
+function calculator(op, numb1, numb2){
+    var result = '';
+    switch(op){
+        case "+":
+            result = numb1 + numb2;
+            break;
+        case "-":
+            result = numb1 - numb2;
+            break;
+        case "*":
+            result = numb1 * numb2;
+            break;
+        case "/":
+            result = numb1 / numb2;
+            break;
+        default:
+            result = '잘못된 연산자 입니다.';
+            break;
+    }
+    return result;
+}
+function calculatorIF(op, numb1, numb2){
+    var result = '';
+    var firstNumb = parseInt(numb1);
+    var secondNumb = parseInt(numb2);
+    if(op == '+'){
+        result = firstNumb + secondNumb;
+    }else if (op == '-'){
+        result = firstNumb - secondNumb;
+    }else if (op == '*'){
+        result = firstNumb * secondNumb;
+    }else if (op == '/'){
+        result = firstNumb / secondNumb;
+    }else{
+        result = "잘못된 연산자 입니다.";
+    }
+    return result;
+}

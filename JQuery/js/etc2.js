3b2a2ac9c1515bc960d98ff4dac87dcf8d293fe5
Mark <teamofmark@gmail.com>
2020-12-07 10:54:06 +0900

    첫번째 예제 완성

--- JQuery/js/etc2.js
+++ JQuery/js/etc2.js
@@ -1,4 +1,4 @@
-$(document).write(function(){
+$(document).ready(function(){
     // *exp.1 원 움직이기
     moveCircle();
 
@@ -10,7 +10,11 @@ function moveCircle(){
     var circle = $(".circle");
     $("#btnMoveCircle").click(function(){
         var xpos = window.prompt("0부터 380이하의 숫자만 입력하세요.");
-
-        $(circle).css("left",xpos);
+        xpos = parseInt(xpos);
+        if(xpos >= 0 && xpos <= 380){
+            $(circle).css("left",xpos);
+        }else{
+            alert('잘못된 수치입니다. 0~380숫자만 입력하세요');
+        }
     });
 }

0312714f66ee4e72221f72df66c96e32457bb6c0
Mark <teamofmark@gmail.com>
2020-12-08 11:51:14 +0900

    실습예제 코드 배포

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     imgAlignHori();
+    imgAlignMix();
 });
 
 function imgAlignHori(){
@@ -19,4 +20,34 @@ function imgAlignHori(){
     $(".reset").click(function(){
         img.css("left",0);
     });
+}
+
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+
+        for (var i = 0; i < imgLength; i++) {
+            var image = img.eq(i);
+            xpos = (i%3)*imgHeight;//? 0/3 = 나머지 0, 1/3 = 1, 2/3 = 2, 3/3 = 0 ....*imgHeight
+            //! 나눗셈 나머지? = 몫은 정수상태여야 하고, 뺼수없는 소수가 되었을때 나오는 나머지.
+            ypos = parseInt(i/3) * imgHeight;//? 0/3 = 0 , 1/3 = 0, 2/3 = 0, 3/3 = 1, 3/4 = 0, 3/5 = 0... *imgHeight
+
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left": 0,
+            "top" : 0
+    });
+    });
+    
 }

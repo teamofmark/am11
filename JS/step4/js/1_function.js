c4ccf7915fe2611b03482098be2af8a20c3e9173
Mark <teamofmark@gmail.com>
2021-03-25 12:50:28 +0900

    매개변수 함수전이 까지 완료

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     // alert(sumAll(1,2,3,4,5,6,7,8,9,10));
     // showInfo('Mark',99); //? ['Mark',99];
     // alert(f(3));
-    alert(sumReturn1(10,20));
+    // alert(sumReturn1(10,20));
 });
 function helloFunc(){
     var comment = '안녕. 방가와!';
@@ -73,4 +73,122 @@ function f(x){
 function sumReturn1(num1,num2){
     var result = num1 + num2;
     return result;
+}
+
+// *case.6 무한루프를 돌며 숫자를 입력받고 입력받은 수의 합을 화면에 출력하는 함수.
+// !단 입력값이 0일경우 즉시 실행중지.
+
+function infiniteSum(){
+    var sum = 0;
+    var count = 1;
+
+    while(true){
+        var value = parseInt(window.prompt('숫자만입력해라'));
+        if(value == 0){
+            document.write('종료');
+            return;
+        }
+
+        sum += value;
+        document.write(count+". " + sum + "<br>");
+        count ++;
+    }
+    document.write('총 ' + count + '번 실행함.');
+
+
+}
+
+// todo.1 구구단출력을 함수로 만들기.
+function printGugudan(){
+    for (var i = 2; i <= 9; i++){
+        document.write(i + '단 출력', "<br>");
+        for(var m = 1; m <= 9; m++){
+            document.write(i + 'x' + m + '=' + (i*m), '<br>' );
+        }
+        document.write('<br>');
+    }
+}
+// *case.7 다음실행구문으로 전달받은 매개변수로 계산하여 결과를 출력하는 함수.
+/*
+    ? calculator("+ - * /",20,10);
+*/
+function calculator(op,numb1,numb2){
+    var result = '';
+    switch(op){
+        case "+":
+            result = numb1 + numb2;
+            break;
+        case "-":
+            result = numb1 - numb2;
+            break;
+        case "*":
+            result = numb1 * numb2;
+            break;
+        case "/":
+            result = numb1 / numb2;
+            break;
+        default:
+            result = '잘못된 연산식 입니다.';
+            break;
+    }
+    return result;
+}
+
+// todo.2 위의 예제에 추가로 사칙연산부분을 함수로 분리해서 조금 더 편하게 사용하게 만들기.
+/*
+    ? calculatorCustom("+ - * /",20,10);
+    ? add(20,10); 더하기작동
+    ? sub(20,10); 빼기 작동
+    ? mul(20,10); 곱하기 작동
+    ? div(20,10); 나누기 작동
+*/
+function calculatorCustom(op,numb1,numb2){
+    var result = '';
+    switch(op){
+        case "+":
+            result = add(numb1,numb2);
+            break;
+        case "-":
+            result = sub(numb1,numb2);
+            break;
+        case "*":
+            result = mul(numb1,numb2);
+            break;
+        case "/":
+            result = div(numb1,numb2);
+            break;
+        default:
+            result = '잘못된 연산식 입니다.';
+            break;
+    }
+    return result;
+}
+
+function add(numb1, numb2){
+   return numb1 + numb2;
+}
+function sub(numb1, numb2){
+    return numb1 - numb2;
+}
+function mul(numb1, numb2){
+    return numb1 * numb2;
+}
+function div(numb1, numb2){
+    return numb1 / numb2;
+}
+// *case.8  함수를 변수에 담기.
+function hello(name){
+    console.log(name + '환영합니다!');
+}
+var func = hello;
+
+// *case.9 매개변수 값으로 함수 사용하기.
+function hi1(){
+    console.log('Hello.');
+}
+function hi2(){
+    console.log('안녕하세요.');
+}
+function execute(func){
+    func();
 }

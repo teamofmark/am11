d8ee6a7b0e6e8a507bc878024aee078e73e36bf4
Mark <teamofmark@gmail.com>
2021-03-30 12:10:47 +0900

    기본선택자와 루틴의 이해

--- JQuery/js/etc1.js
+++ JQuery/js/etc1.js
@@ -3,16 +3,27 @@ $(document).ready(function(){
     $divs.css("color","#f00");
     sayHello();
     addBorder();
+    textChange();
 });
 
+// *case.1
 function sayHello(){
     $("#btnCheck").click(function(){
         alert('hello?');
     });
 }
-
+// *case.2
 function addBorder(){
     $("#btnAddBorder").click(function(){
         $("#panel").css("border","5px solid black");
+    });
+}
+// *case.3
+function textChange(){
+    $("#btnTextChange").click(function(){
+        $("#panel2").css({
+            "font-size" : "25px",
+            "color" : "green"
+        });
     });
 }

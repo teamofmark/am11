2be02bf36c3ca666b5745c11630ddcda8712614f
Mark <teamofmark@gmail.com>
2021-03-31 11:53:49 +0900

    circleKeyControl

--- JQuery/js/etc2.js
+++ JQuery/js/etc2.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     moveCircle();
     detailMoveCircle();
+    keyControl();
 });
 
 // *case.1 원 x좌표로 움직이기
@@ -36,5 +37,48 @@ function detailMoveCircle(){
                 "top" : ypos
             });
         }
+    });
+}
+// *case.3 key control을 통해 원 움직이기
+function keyControl(){
+    var circle = $(".circleKey");
+    var currentXpos = 0;
+    var currentYpos = 0;
+    var range = 50;
+    // ?선언부 종료
+
+    $(document).keydown(function(e){
+        // console.log('입력한 키 코드?' + e.keyCode);  
+        switch(e.keyCode){
+            case 87:
+                currentYpos -= range;
+                break;
+            case 83:
+                currentYpos += range;
+                break;
+            case 65:
+                currentXpos -= range;
+                break;
+            case 68:
+                currentXpos += range;
+                break;
+            default:
+                console.log('잘못된 키입력입니다.');
+                break;
+        }
+        if(currentXpos < 0){
+            currentXpos = 0;
+        }
+        if(currentXpos > 380){
+            currentXpos = 380;
+        }
+        if(currentYpos < 0){
+            currentYpos = 0;
+        }
+        if(currentYpos > 380){
+            currentYpos = 380;
+        }
+        circle.css("left",currentXpos);
+        circle.css("top",currentYpos);
     });
 }

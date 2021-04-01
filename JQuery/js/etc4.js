7533c2fe8862108025acb4eee97fa821475642db
Mark <teamofmark@gmail.com>
2021-04-01 12:51:41 +0900

    ETC4번 풀이종료

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     imgAlignHori();
+    imgAlignMix();
 });
 
 // *case.1
@@ -18,5 +19,33 @@ function imgAlignHori(){
     });
     $('.reset').click(function(){
         img.css("left", 0);
+    });
+}
+
+// *case.2
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0; 
+    var ypos = 0;
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+
+        for (var i = 0; i < imgLength; i++){
+            var image = img.eq(i); //?
+            xpos = (i%3)*imgHeight;//?
+            ypos = parseInt(i/3) * imgHeight; //?
+
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        });
     });
 }

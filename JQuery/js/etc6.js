8d3e8d1eeb2bd6015534b67df8d1023cce48f771
Mark <teamofmark@gmail.com>
2021-04-01 12:54:29 +0900

    ETC6번 과제 배포

---
+++ JQuery/js/etc6.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    gallery(0,100,100);
+    $(".1way").click(function(){gallery(1,1000,1000)});
+    $(".2way").click(function(){gallery(2,500,500)});
+    $(".3way").click(function(){gallery(3,333,333)});
+    $(".4way").click(function(){gallery(4,250,250)});
+    $(".5way").click(function(){gallery(5,200,200)});
+    $(".6way").click(function(){gallery(6,166,166)});
+    $(".7way").click(function(){gallery(7,142,142)});
+    $(".8way").click(function(){gallery(8,125,125)});
+    $(".9way").click(function(){gallery(9,111,111)});
+    $(".10way").click(function(){gallery(10,100,100)});
+});

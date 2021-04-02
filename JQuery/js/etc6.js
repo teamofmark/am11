df98905e825522446f30955d3c9315fb99fef892
Mark <teamofmark@gmail.com>
2021-04-02 11:07:48 +0900

    과제풀이

--- JQuery/js/etc6.js
+++ JQuery/js/etc6.js
@@ -11,3 +11,23 @@ $(document).ready(function(){
     $(".9way").click(function(){gallery(9,111,111)});
     $(".10way").click(function(){gallery(10,100,100)});
 });
+function gallery(count, imgWidth, imgHeight){
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+
+    var xpos, ypos;
+
+    for(i = 0; i<$imgLength; i++){
+        var $image = $images.eq(i); //? eq = DOM 요소 세트를 나타내는 jQuery 객체가 주어지면 .eq()메소드는 해당 세트 내의 한 요소에서 새 jQuery 객체를 구성한다.
+        
+        xpos = (i % count) * imgWidth;
+        ypos = parseInt(i / count) * imgHeight;
+
+        $image.css({
+            'left' : xpos,
+            'top' : ypos,
+            'width' : imgWidth,
+            'height' : imgHeight
+        })
+    }
+}

31a6eba6de9a6380404948e3b88c472dff42c807
Mark <teamofmark@gmail.com>
2021-04-02 12:54:42 +0900

    게임 함수구조분리 작업

--- JQuery/js/etc7.js
+++ JQuery/js/etc7.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 });
 function tabMenu(currentTab){
     var $selectMenu = null; // ? click한 tabMenu
-    $(currentTab).click(function(){ //? 클로저함수 - 내부에서 selectMenu 변수 사용중.
+    $(currentTab).click(function(){ //? 클로저함수 - 내부에서 selectMenu 변수 사용중
         if($selectMenu != null){
             $selectMenu.removeClass("activated");
         }
---
+++ JQuery/js/etc9.js
@@ -0,0 +1,49 @@
+var count = 0;
+var $score = null;
+var $circle = null;
+var $btnStart = null;
+var $panel = null;
+var playState = false;
+var timerID = 0;
+
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $btnStart = $("#btn_start");
+    $panel = $(".panel");
+}
+function initEvent(){
+    $btnStart.click(function(){
+        gameStart();
+    });
+    $circle.click(function(){
+        scoreCount();
+    });
+}
+function gameStart(){
+    playState = true;
+    if(playState == true){
+        gameEnd();
+
+        timerID = setInterval(moveCircle,500);
+    }
+}
+function moveCircle(){
+    $circle.css({
+        left : Math.floor(Math.random()*($panel.width() - $circle.width())),
+        top: Math.floor(Math.random()*($panel.height() - $circle.height()))
+    });
+}
+function scoreCount(){
+    if(playState == true){
+        count ++;
+        $score.text(count);
+    }
+}
+function gameEnd(){
+
+}

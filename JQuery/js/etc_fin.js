29e9b53f03aa2386285e47b7dd0372f3bdea0219
Mark <teamofmark@gmail.com>
2021-04-06 12:27:09 +0900

    JS/JQ최종과제배포

---
+++ JQuery/js/etc_fin.js
@@ -0,0 +1,37 @@
+/*
+    todo : 물고기 경주 게임
+
+    ? 요구사항
+    * 시작 버튼을 누르면 경기가 시작한다. (2초간격 움직임)
+    * 물고기는 실제 경주 게임처럼 어떤 물고기가 1등이 될 것인지 예측할 수 없게 할 것.
+    * 물고기가 골인 지점을 통과하면 몇 번째 물고기가 우승했는지 알려주고 게임을 종료.
+*/
+$(document).ready(function(){
+    init();
+    fishStartPosition();
+    initEvent();
+});
+function init(){ //? 변수선언부
+
+}
+function fishStartPosition(){ //? 물고기 초기 배치.
+
+}
+function initEvent(){ //? event 처리함수
+
+}
+function startGame(){ //? game 시작함수
+
+} 
+function updateFishPosition(){ //?물고기 움직임 함수
+
+}
+function displayFishPositionInfo(){ //?물고기 위치 출력함수
+
+}
+function checkGoalFish(){ //? 결승선 통과 물고기 검사 함수
+
+}
+function endGame(){ //? 게임종료 함수
+
+}

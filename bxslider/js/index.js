ddbcfe8832861168f4340eb9548674d7f9b8d9b0
Mark <teamofmark@gmail.com>
2021-04-07 12:30:23 +0900

    fitvids 배포

--- bxslider/js/index.js
+++ bxslider/js/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    $('.slider').bxSlider();
+    $('.slider').bxSlider({
+        mode : 'horizontal',
+        speed : 500,
+        startSlide: 0,
+        randomStart : false,
+        infiniteLoop : false,
+        hideControlOnEnd : true,
+        easing: 'ease-in-out',
+        captions: true,
+        ticker: false,
+        tickerHover: false,
+        adaptiveHeight: true,
+        video: true
+    });
 });

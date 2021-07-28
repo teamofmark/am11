67d8189634b0cb6e80c9a744f56827fa383b53e5
Mark <teamofmark@gmail.com>
2021-07-28 12:47:58 +0900

    2번예제 함수분리화완료

--- JQuery/js/etc2.js
+++ JQuery/js/etc2.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     // *case.1
     moveCircle();
+    // *case.2
+    // detailMoveCircle();
+    detailMoveCircleExt();
 });
 
 // *case.1 prompt를 이용한 원 움직이기 (xAxis)
@@ -15,4 +18,52 @@ function moveCircle(){
             alert("잘못된 수치입니다.");
         }
     });
+}
+
+// *case.2 input Value를 이용한 원 움직이기(xAxis, yAxis)
+function detailMoveCircle(){
+    var $circle = $(".circleDetail");
+    $("#btnDetailMove").click(function(){
+        var xpos = $("#xpos").val();
+        var ypos = $("#ypos").val();
+
+        xpos = parseInt(xpos);
+        ypos = parseInt(ypos);
+
+        if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+            alert('잘못된 수치입니다. 0 ~ 380이내로 입력하세요.');
+        }else{
+            $circle.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+}
+// *case.2 함수구조분리
+function detailMoveCircleExt(){
+    var $circle = null;
+
+    circleInit();
+    $("#btnDetailMove").click(circleEvent);
+}
+function circleInit(){
+    $circle = $(".circleDetail");
+}
+function circleEvent(){
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+    circleMovCommand(xpos, ypos);
+}
+function circleMovCommand(xpos, ypos){
+    if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+        alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
+    }else{
+        $circle.css({
+            "left" : xpos,
+            "top" : ypos
+        });
+    }
 }

26aa31daa84eb582efc440cc0797bde8ae380c2d
Mark <teamofmark@gmail.com>
2021-07-28 11:21:57 +0900

    선택자 실습

--- JQuery/js/etc1.js
+++ JQuery/js/etc1.js
@@ -2,10 +2,29 @@ $(document).ready(function(){
     // *case.1 
     var $divs = $("div");
     $divs.css("color","#f00");
+
+    // *case.2
     $("#btnCheck").click(sayHello);
+
+    // *case.3
+    $("#btnAddBorder").click(addBorder);
+    // addBorder();
 });
 
 // *case.2
 function sayHello(){
     alert('hello');
+}
+
+// *case.3
+function addBorder(){
+    $("#panel").css("border","5px solid black");
+    // $("#btnAddBorder").click(function(){
+        // $("#panel").css("border","5px solid black");
+    // });
+}
+
+// todo.
+function textChange(){
+    
 }

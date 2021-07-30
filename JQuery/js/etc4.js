53573efb83be1b9616ebeb3b87b0f8b035e8a86f
Mark <teamofmark@gmail.com>
2021-07-30 12:24:32 +0900

    etc4. 이미지제어 종료

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     // *case. 1 
     imgAlignHori();
 
+    // *case. 2
+    imgAlignMix();
 
 });
 // *case. 1 정렬버튼을 눌러서 이미지 가로로 배치하기. (개수가 추가되면 알아서 늘어나기)
@@ -21,4 +23,33 @@ function imgAlignHori(){
     $(".reset").click(function(){
         img.css("left", 0);
     })
+}
+
+// *case. 2 정렬버튼을 눌러서 이미지 3단배열로 배치하기. (개수가 추가되면 알아서 늘어나기)
+
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%3) * imgHeight;
+            ypos = parseInt(i/3) * imgHeight;
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        })
+    });
 }

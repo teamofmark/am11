1ed6f73f8c44141d9fe0823e7f27e16a726e83bf
Mark <teamofmark@gmail.com>
2021-08-04 13:00:01 +0900

    endGame 직전

--- JQuery/js/etc15.js
+++ JQuery/js/etc15.js
@@ -60,3 +60,40 @@ function updateFishPosition(){
     }
 }
 // ? 물고기 움직임 함수
+function displayFishPositionInfo(){
+    var info = "";
+    for (var i = 0; i < i< $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+
+        info += i + "번 물고기 : " + $fish.position().left + "px <br>";
+    }
+    $info.html(info);
+}
+// ? 전광판. 물고기 위치 출력 함수
+
+function checkGoalFish(){
+    var winnerList = [];
+
+    for(var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+                index: (i), position:fishCurrentPosition
+            });
+        }
+    }
+    if(winnerList.length > 0){
+        winnerList.sort(function(a,b){
+            return b.position-a.position;
+            /*
+                ! arr.sort([compareFunction]);
+                ! 매개변수 = compareFunction - 정렬순서 정의 함수. 생략시 오름차순 처리 가능.
+                *(문자열변환 > 유니코드값의 크기에 따라 정렬)
+                ! 리턴값 = 정렬이 완료된 결과 값.
+            */
+        });
+        endGame();
+        alert('우승 !' + winnerList[0].index + '번 물고기!');
+    }
+}

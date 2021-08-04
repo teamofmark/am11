23976326c514af81e22d5d2f48ff1f2a2eecb1da
Mark <teamofmark@gmail.com>
2021-08-04 11:37:02 +0900

    randompos game 완성

--- JQuery/js/etc14.js
+++ JQuery/js/etc14.js
@@ -8,7 +8,7 @@ var timerID = 0;
 
 $(document).ready(function(){
     init();
-
+    initEvent();
 });
 function init(){
     $score = $("#score");
@@ -23,4 +23,30 @@ function initEvent(){
     $circle.click(function(){
         scoreCount();
     });
+}
+function gameStart(){
+    playState = true;
+    if(playState == true){
+        gameEnd();
+        timerID = setInterval(moveCircle, 500);
+    }
+}
+function moveCircle(){
+    $circle.css({
+        left: Math.floor(Math.random()*($panel.width() - $circle.width())),
+        top: Math.floor(Math.random()*($panel.height() - $circle.height()))
+    });
+}
+function scoreCount(){
+    if(playState == true){
+        count ++;
+        $score.text(count);
+    }
+}
+function gameEnd(){
+    setTimeout(function(){
+        playState = false;
+        clearInterval(timerID);
+        alert('게임종료');
+    }, 10000);
 }

274967a976c2bbd6f91f2542156d1b0ea7f2cf95
Mark <teamofmark@gmail.com>
2021-08-12 12:21:51 +0900

    repo REst

--- JQuery/js/etc5.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    imgAlignCustom(8);
-    //? 8단 갤러리로 작동.
-});
-// todo. 위 함수로 실행될 수 있도록 logic 작성.
--- JS/step1/js/1_1_check.js
+++
@@ -1,9 +0,0 @@
-function runAlert(){
-    alert('hello, js!');
-}
-function runWrite(){
-    document.write('hi' + '<br>' + 'js...!');
-}
-function runLog(){
-    console.log('hi \n js...!');
-}

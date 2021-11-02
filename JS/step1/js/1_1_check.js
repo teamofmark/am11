27757c62a539790c3b5465c9deec82955faef897
Mark <teamofmark@gmail.com>
2021-11-02 12:47:58 +0900

    check부분만진행

---
+++ JS/step1/js/1_1_check.js
@@ -0,0 +1,12 @@
+function runAlert(){
+    alert('target');
+}
+function runWrite(){
+    document.write('hi' + '<br>' + 'javascript');
+}
+function runLog(){
+    console.log('hi \n javascript');
+}
+
+
+// ? 우리가 만든 함수인가?  함수 = 기능. 함수 = 이미 만들어진것(라이브러리), 내가 만드는 것(임의).

969de90fbf5cb12a86157f9086c6489eb8094de6
Mark <teamofmark@gmail.com>
2021-11-15 11:24:49 +0900

    return 과 break의 차이 설명중

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -32,3 +32,38 @@ function showInfo(){
     console.log("index[2] = " + arguments[2]);
     alert(arguments[0] + '의 나이는' + arguments[1] + '이고 혈액형은 ' + arguments[2] + '입니다.');
 }
+/*
+    ? return
+    * 함수내부는 함수라는 범위 안에 갇히기 때문에, 한번 실행되면 함수 외부에서 접근 할수 없다.
+    * 매개변수 활용으로 함수내부에 data 전달 가능. 리턴값은 이와 반대로 함수외부로 data 전달 가능.
+
+    function f(x){
+        return x * x;
+    }
+    alert(f(3));
+*/
+function sumReturn1(num1,num2){
+    var result = num1 + num2;
+    return result; //? 외부로 나가는 data.
+}
+
+// *case.4 무한루프를 돌며 숫자를 입력받고 입력받은 수의 합을 화면에 출력하는 함수.
+// ! 단 입력값이 0이면 즉시 실행중지.
+
+function infiniteSum(){
+    var sum = 0;
+    var count = 1;
+
+    while(true){
+        var value = parseInt(window.prompt('숫자만 입력해요.'));
+        if(value == 0){
+            document.write('종료');
+            break;
+        }
+
+        sum += value;
+        document.write(count + '. ' + sum + '<br>');
+        count++;
+    }
+    document.write('총' + count + '번 실행함.');
+}

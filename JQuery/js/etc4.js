e2e9d9ae21b106b072901d4171330955f7403382
Mark <teamofmark@gmail.com>
2021-11-19 12:39:05 +0900

    alignHori 완성

---
+++ JQuery/js/etc4.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // *case.1 
+    imgAlignHori();
+});
+function imgAlignHori(){
+    var img = $(".imgContainer img");
+    var xpos = 0;
+    $(".alignHori").click(function(){
+        var imgLength = img.length;
+        var imgWidth = img.width();
+        for (var i = 0; i < imgLength; i++){
+            var image = img.eq(i); //? (n)번째 target 부르기.
+            xpos = i * imgWidth;
+            image.css("left", xpos);            
+        }
+    });
+    $(".reset").click(function(){
+        img.css("left", 0);
+    });
+}

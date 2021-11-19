543e4b9b117592650413f90b401b21909f11e632
Mark <teamofmark@gmail.com>
2021-11-19 12:43:11 +0900

    3열로 배치되는 갤러리 실습 파일

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     // *case.1 
     imgAlignHori();
+
+    //*case.2
+    imgAlignMix();
 });
 function imgAlignHori(){
     var img = $(".imgContainer img");
@@ -17,4 +20,22 @@ function imgAlignHori(){
     $(".reset").click(function(){
         img.css("left", 0);
     });
+}
+function imgAlignMix(){
+//! 실행버튼(.alignMix) 클릭했을 때
+     //? img 갯수 불러오기
+     //? img 높이 불러오기
+
+    //? 반복문. img갯수보다 작을 때 까지만
+
+    //? n번째 img  선택하기
+
+    //? xpos 연산식 - hint: 나머지
+
+    //? ypos 연산식 - hint: 몫
+
+    //? css 적용부
+//! 초기화버튼(.resetMix) 클릭했을 때
+    //? css 초기화 (image 원위치)
+
 }

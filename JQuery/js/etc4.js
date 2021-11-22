aef70b1b6aa01f15b992084d2cbb22cd95c437f6
Mark <teamofmark@gmail.com>
2021-11-22 11:10:05 +0900

    문제풀이

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -22,20 +22,39 @@ function imgAlignHori(){
     });
 }
 function imgAlignMix(){
-//! 실행버튼(.alignMix) 클릭했을 때
-     //? img 갯수 불러오기
-     //? img 높이 불러오기
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
 
-    //? 반복문. img갯수보다 작을 때 까지만
+    $(".alignMix").click(function(){ //! 실행버튼(.alignMix) 클릭했을 때 
+        var imgLength = img.length; //? img 갯수 불러오기
+        var imgHeight = img.height(); //? img 높이 불러오기
 
-    //? n번째 img  선택하기
+        for (var i = 0; i < imgLength; i++){ //? 반복문. img갯수보다 작을 때 까지만
+            var image = img.eq(i); //? n번째 img  선택하기
+            xpos = (i%3)*imgHeight; //? xpos 연산식 - hint: 나머지
+            //? 0(0), 1(1), 2(2),
+            //? 3(0), 4(1), 5(2),
+            //? 6(0), 7(1), 8(2)....
+            ypos = parseInt(i/3) * imgHeight;  //? ypos 연산식 - hint: 몫
+            //? 0(0), 1(0), 2(0),
+            //? 3(1), 4(1), 5(1),
+            //? 6(2), 7(2), 8(2)....
+            image.css({
+                //? css 적용부
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){//! 초기화버튼(.resetMix) 클릭했을 때
+        img.css({//? css 초기화 (image 원위치)
+            "left" : 0,
+            "top" : 0
+        });
+    });
+    
 
-    //? xpos 연산식 - hint: 나머지
-
-    //? ypos 연산식 - hint: 몫
-
-    //? css 적용부
-//! 초기화버튼(.resetMix) 클릭했을 때
-    //? css 초기화 (image 원위치)
+    
 
 }

ba1c5588a64da29ba94dcc47ea75098eda8ef493
Mark <teamofmark@gmail.com>
2021-11-23 11:46:16 +0900

    왕복운동 분리없이 제작

--- JQuery/js/etc10.js
+++ JQuery/js/etc10.js
@@ -1,3 +1,34 @@
-$(document).ready(function(){
 
+
+$(document).ready(function(){
+    var $circle = $("#circle");
+    var runStep = 10; //? 한번 움직일때의 간격
+    var xpos = $circle.position().left;
+    var railWidth = $("#rail").width();
+    var timerID = 0;
+
+    startMove($circle, runStep, xpos, railWidth, timerID);
 });
+
+function startMove($circle, runStep, xpos, railWidth, timerID){
+    $("#btn_start").click(function(){
+        timerID = setInterval(function(){
+            xpos += runStep;
+
+            $circle.css({
+                left: xpos
+            });
+
+            if(xpos > railWidth || xpos < 0){
+                runStep *= -1;
+            }
+        },0);
+        return stopMove(timerID);
+    });
+    //? setInterval = 무한으로 실행(특정시간차)
+}
+function stopMove(timerID){
+    $("#btn_stop").click(function(){
+        clearInterval(timerID);
+    });
+}

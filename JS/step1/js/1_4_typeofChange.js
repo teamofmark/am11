69a0ca9c2ab7e9f45c7c1f60ab97e4feb44a7f53
Mark <teamofmark@gmail.com>
2022-03-16 12:21:23 +0900

    todo 풀이

--- JS/step1/js/1_4_typeofChange.js
+++ JS/step1/js/1_4_typeofChange.js
@@ -88,19 +88,19 @@ function numbString(){
 // todo. 1-1 암시적형변환
 function todo1(){
     var test = 100;
-    var result = test; //? 형변환처리하기
+    var result = test + ''; //? 형변환처리하기
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 1-2 String 형변환
 function todo2(){
     var test = 100;
-    var result = test; //? 형변환처리하기
+    var result = String(test); //? 형변환처리하기
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 1-3 toString 형변환
 function todo3(){
     var test = 100;
-    var result = test; //? 형변환처리하기
+    var result = test.toString(); //? 형변환처리하기
     console.log(typeof(result) + ' = ' + result);
 }
 
@@ -109,12 +109,12 @@ function todo3(){
 // todo. 2-1 parse 형변환
 function todo4(){
     var test = "100.51";
-    var result = test + 150 + "20"; //? 형변환해서 연산가능하게 처리하기.
+    var result = parseFloat(test) + 150 + parseInt("20"); //? 형변환해서 연산가능하게 처리하기.
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 2-2 Number 형변환
 function todo5(){
     var test = "100.51";
-    var result = test + 150 + "20"; //? 형변환해서 연산가능하게 처리하기.
+    var result = Number(test) + 150 + Number("20"); //? 형변환해서 연산가능하게 처리하기.
     console.log(typeof(result) + ' = ' + result);
 }

0df73839831e31646a3aead1fa3ea7db709a235b
markStark <teamofmark@gmail.com>
2022-03-24 11:37:37 +0900

    문제 배포

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -82,3 +82,23 @@ function printGugudan(){
         document.write("<br>");
     }
 }
+
+// todo.2 다음 실행구문으로 전달받은 매개변수로 계산하여 결과를 출력하는 계산기 함수 만들기.
+
+/*
+    //* 실행구문
+    * document.write("1 결과 =" + calculator("+", 20, 10));
+    * document.write("2 결과 =" + calculator("-", 20, 10));
+    * document.write("3 결과 =" + calculator("*", 20, 10));
+    * document.write("4 결과 =" + calculator("/", 20, 10));
+    * document.write("5 결과 =" + calculator("%", 20, 10));
+    //* 결과 
+    * 결과 = 30
+    * 결과 = 10
+    * 결과 = 200
+    * 결과 = 2
+    * 결과 = 잘못된 연산자 입니다.
+*/
+function calculator(){
+    
+}

89b040e3c89764dada6eddf5e800130aa2f3c821
Mark <teamofmark@gmail.com>
2022-03-29 11:32:32 +0900

    component만들기 todo

---
+++ JQuery/js/etc3.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    toggleCheck();
+    // todo. 자신만의 component 한개 만들기.
+    toggleTodo();
+});
+function toggleCheck(){
+    var $checkTarget = $(".material-icons.checkIco");
+    var $checkLabel = $(".label.checkLabel");
+    var checkStatus = false;
+
+    $checkLabel.click(function(){
+        checkStatus = !checkStatus;
+        if(checkStatus == true){
+            $checkTarget.text("check_box");
+        }else{
+            $checkTarget.text("check_box_outline_blank");
+        }
+    });
+}
+
+function toggleTodo(){
+
+}

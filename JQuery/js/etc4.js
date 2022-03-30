25cc168178910c00e212c95cebb1d921e88f3d04
Mark <teamofmark@gmail.com>
2022-03-30 12:23:10 +0900

    실습문제 풀이

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -21,7 +21,43 @@ function imgAlignHori(){
     });
 }
 function imgAlignMix(){
-    // ? x좌표,y좌표
-    // ? -> x의 좌표는 0,1,2번 img는 0,1,2를 곱하지만 3에서는 다시 0이 되어야 한다.
-    // ? -> y의 좌표는 0,1,2번 img는 0,0,0,을 곱하지만 3부터 5까지는 1을 곱해야 한다.
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+        // ? -> x의 좌표는 0,1,2번 img는 0,1,2를 곱하지만 3에서는 다시 0이 되어야 한다.
+        /*
+            * 0(0), 1(1), 2(2)
+            * 3(0), 4(1), 5(2)
+            * 6(0), 7(1), 8(2)
+            * 9(0), 10(1), 11(2)
+            * 12(0).............
+        */
+        // ? -> y의 좌표는 0,1,2번 img는 0,0,0,을 곱하지만 3부터 5까지는 1을 곱해야 한다.
+        /*
+            * 0(0), 1(0), 2(0)
+            * 3(1), 4(1), 5(1)
+            * 6(2),
+            * 9(3)
+        */
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%3) * imgHeight;
+            ypos = parseInt(i/3) * imgHeight;
+            image.css({
+                'left' : xpos,
+                'top' : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        });
+    });
+
 }

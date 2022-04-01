4df50cc8aa3d7e5b6978bfae0f31572530dc2755
Mark <teamofmark@gmail.com>
2022-04-01 12:26:10 +0900

    interval,clear 함수구조분리

--- JQuery/js/etc10.js
+++ JQuery/js/etc10.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
     // ? 움직일 대상, 움직이는 간격, x좌표를 저장할 곳, 막대 길이.
     var $circle = $("#circle");
@@ -28,4 +29,43 @@ function stopMove(timerID){
     $("#btn_stop").click(function(){
         clearInterval(timerID);
     });
+}
+*/
+var $circle = null;
+var railWidth = null;
+var runStep = 10;
+var timerID = 0;
+// ? document 전체에서 사용할 변수.
+
+$(document).ready(function(){
+    init(); //? 대상 및 변수 불러오기.
+    initEvent(); //? event 함수.
+});
+function init(){
+    $circle = $("#circle");
+    railWidth = $("#rail").width();
+}
+
+function initEvent(){
+    $("#btn_start").click(moveStart);// ? 움직이는 기능 함수
+    $("#btn_stop").click(moveStop);// ? 멈추는 기능 함수
+}
+function moveStart(){
+    if(timerID == 0){
+        timerID = setInterval(moveCircle,0);// ? 움직임logic
+    }
+}
+function moveCircle(){
+    var xpos = $circle.position().left;
+    xpos += runStep;
+    $circle.css({
+        left: xpos
+    });
+    if(xpos > railWidth || xpos < 0){
+        runStep *= -1;
+    }
+}
+function moveStop(){
+    clearInterval(timerID); //? timerID clear
+    timerID = 0; //? timerID 초기화
 }

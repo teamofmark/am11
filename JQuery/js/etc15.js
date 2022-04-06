c47014c24b8cebd4503ce983344eb110e14edb5b
Mark <teamofmark@gmail.com>
2022-04-06 11:40:08 +0900

    결승선감지부분 완성

--- JQuery/js/etc15.js
+++ JQuery/js/etc15.js
@@ -2,7 +2,7 @@
 var $fishList = null;
 var timerID = -1;
 var $info = null;
-var goalline = 0;
+var $goalline = 0;
 var $fishWidth = 0;
 
 $(document).ready(function(){
@@ -36,7 +36,7 @@ function startGame(){
         timerID = setInterval(function(){
             updateFishPosition();// ? 물고기 위치 랜덤 변화
             displayFishPositionInfo();// ? 변화된 좌표값 출력
-            // ? 결승선 도달 감지(종료함수)
+            checkGoalFish();// ? 결승선 도달 감지(종료함수)
         },200);
     }
 }
@@ -52,6 +52,7 @@ function updateFishPosition(){
 }
 // todo. 물고기 위치 출력함수 만들기
 function displayFishPositionInfo(){
+    console.log('2.정보출력');
     var infoText = "";
 
     for(var i = 0; i < $fishList.length; i++){
@@ -60,4 +61,36 @@ function displayFishPositionInfo(){
     }
 
     $info.html(infoText);// ? 출력부 - 전광판 아이디 불러다 값넣어주기.
+}
+
+function checkGoalFish(){
+    var winnerList = [];
+    for(var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+
+        if(fishCurrentPosition >= $goalline){
+            winnerList.push({
+                index: (i), position:fishCurrentPosition
+            });
+        }
+    }
+    // ? goalLine을 넘은 물고기를 기준으로 winnerList 배열에 position이라는 이름으로 저장
+
+    if(winnerList.length>0){
+        winnerList.sort(function(a,b){
+            return b.position - a.position; //? position으로 저장된 값 기준 내림차순 정렬.
+        });
+        /*
+             ? 배열(data).sort([compareFunction]);
+             ! 매개변수 = compareFunction - 정렬순서 정의 함수(개발자). 생략시 오름차순정리.(unicodeValue)
+        */
+       endGame();
+       alert('우승 !' + winnerList[0].index + '번 물고기!');
+    }
+}
+
+function endGame(){
+    console.log('4.게임종료');
+    clearInterval(timerID);
+    timerID = -1;
 }

0e92eeedaa6eb502c2a5bff76f11d698f69c815c
Mark <teamofmark@gmail.com>
2022-07-12 12:45:27 +0900

    check 하는 방식 3가지

---
+++ JS/step1/js/1_1_check.js
@@ -0,0 +1,10 @@
+function alertCheck(){
+    // ? 함수(alertCheck)를 생성하였다.
+    alert('검사할 대상');
+}
+function writeCheck(){
+    document.write('검사할 대상');
+}
+function logCheck(){
+    console.log('검사할 대상');
+}

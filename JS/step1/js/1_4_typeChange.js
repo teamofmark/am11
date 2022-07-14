b624d3aa1b7763f928ab714ded7eeb9b7eb72579
Mark <teamofmark@gmail.com>
2022-07-14 11:44:03 +0900

    형변환 실습문제 5종

--- JS/step1/js/1_4_typeChange.js
+++ JS/step1/js/1_4_typeChange.js
@@ -67,3 +67,45 @@ function bgColorRandom(){
         $("body").css("background-color", "#" + bgColor);
     },1000);
 }
+
+// *case. 6 test에 들어있는 문자를 숫자로 형변환.
+/*
+    ? Number
+    * Number(value) - 정수, 실수 구분없이 형변환.
+
+    ? parse
+    * 1. parseInt(value); - 정수형반환. (ex > '실수'형태로 전달되어도 '정수'형태로 강제변환)
+    * 2. parseFloat(value); - 실수형반환. (ex > '정수'형태로 전달되어도 '실수'형태로 강제변환)
+*/
+function numbString(){
+    var test = '300';
+    var result = parseInt(test) + 10;
+    console.log('result = ' + result);
+}
+// todo. 1 변수 test에 들어있는 숫자 100을 문자형으로 변형. result로 출력(typeof 검수 필수).
+// todo. 1-1 암시적형변환
+function todo1(){
+    
+}
+// todo. 1-2 String 형변환
+function todo2(){
+
+}
+// todo. 1-3 toString 형변환
+function todo3(){
+
+}
+
+// todo. 2 문자 형을 숫자형으로 변환하여 연산이 가능하게 만들기.
+// todo. 2-1 parse 형변환.
+function todo4(){
+    var test = "100.51";
+    var result = test + 150 + "20"; // todo. 형변환 실습대상
+    // todo. typeof로 자료형 검수 및 출력.
+}
+// todo. 2-2 Number 형변환.
+function todo5(){
+    var test = "100.51";
+    var result = test + 150 + "20"; // todo. 형변환 실습대상
+    // todo. typeof로 자료형 검수 및 출력.
+}

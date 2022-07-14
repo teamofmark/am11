c2187420290297f9ef8bf6822301c7e266f0924b
Mark <teamofmark@gmail.com>
2022-07-14 11:34:18 +0900

    16진변환의 예 (toString)

--- JS/step1/js/1_4_typeChange.js
+++ JS/step1/js/1_4_typeChange.js
@@ -18,3 +18,52 @@ function agePlus(){
     var result = Number(age) + 20; //? Number - 전역함수 정,실수 무관 변환.
     console.log(result);
 }
+
+// *case. 2 강제 숫자 -> 문자형 변환.
+function forcedString(){
+    var a = '30';
+    var result = 1 + a + 10;
+    console.log(result);
+}
+// *case. 3 강제 불리언 -> 숫자형 변환.
+function forcedNumber(){
+    var result = 2 + true; //? false = 0, true = 1.
+    console.log(result);
+}
+// *case. 4 강제 불리언 -> 문자형 변환.
+function forcedString2(){
+    var a = '2' + true;
+    console.log(a);
+}
+
+// *case.5 test1에 들어있는 숫자를 문자로 명시적 형변환.
+/*
+    ? String
+    * String(value) -> value를 문자형으로 변환. 
+
+    ? .toString()
+    * numberValue.toString(); - 문자형(진수) 변환.
+
+    ? .toFixed()
+    * numberValue.toFixed(); - 실수(반올림발생) 문자형 변환.
+*/
+function stringNumb(){
+    var test1 = 15;
+    var result = String(test1);
+    console.log(typeof(result) + ' = ' + result);
+}
+function stringtoNumb(){
+    var test1 = 15;
+    var result = test1.toString();
+    console.log(typeof(result) + ' = ' + result);
+}
+// *case. 5-1 random Background-color
+function bgColorRandom(){
+    setInterval(function(){
+        var bgColor = Math.random()*0xfff;
+        bgColor = parseInt(bgColor);
+        bgColor = bgColor.toString(16);
+
+        $("body").css("background-color", "#" + bgColor)
+    },1000);
+}

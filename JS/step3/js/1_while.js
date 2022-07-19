879be89e6ad9e6dfd4452029b45813b5a48e910a
Mark <teamofmark@gmail.com>
2022-07-19 12:04:37 +0900

    while todo 시작

--- JS/step3/js/1_while.js
+++ JS/step3/js/1_while.js
@@ -1,10 +1,10 @@
 /*
     ? while
-    while 반복문은 조건이 참일 경우 무한 반복한다.
-    (물론 조건이 정해진다면 for문 처럼도 사용 가능하지만, 
-        용도를 구분지어 사용하는 것이 좋다.)
-    정해진 횟수만큼의 반복은 for를 사용하지만, 무한반복처리, 파일 읽고쓰기 및 
-    전송(java,php...), DB데이터 출력하기.
+    * while 반복문은 조건이 참일 경우 무한 반복한다.
+    * (물론 조건이 정해진다면 for문 처럼도 사용 가능하지만, 
+    *     용도를 구분지어 사용하는 것이 좋다.)
+    * 정해진 횟수만큼의 반복은 for를 사용하지만, 무한반복처리, 파일 읽고쓰기 및 
+    * 전송(java,php...), DB데이터 출력하기.
 */
 
 // *case.1 password "1234"가 입력되면 "접속승인", 아니면 "잘못입력하였습니다." 무한 출력.
@@ -33,16 +33,16 @@ function whileContinue(){
 function whileBreak(){
     var i = 1;
     while(i <= 10){ //? 총 10회 반복
-        break; //? break로 인해 loop 실행 불가.
         i++;
         document.write(i + '<br>');
+        break; //? break로 인해 loop 실행 불가.
     }
     document.write('최종 i = ' + i + '<br>'); //? 최종결과만 출력.
 }
 // ! continue = 튕겨냄 . break = 즉각적 중단.
 // todo. 구구단 숫자를 입력받아 출력해주는 반복문 만들기.
 function gugudan(){
-
+    // ! 집에서
 }
 // todo. end가 입력될 때 까지 숫자를 입력받고 합을 출력 하되 prompt에는 몇번 수행 했는가에 대한 표시 하기.
 function sumNumb(){

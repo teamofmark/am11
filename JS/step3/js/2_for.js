79d1a31beda13722ffbaa79478007a56e20f0c33
Mark <teamofmark@gmail.com>
2022-07-22 11:37:23 +0900

    pyramid Custom (loop 1) 도 생각해보기

--- JS/step3/js/2_for.js
+++ JS/step3/js/2_for.js
@@ -188,3 +188,13 @@ function pyramidEx(){
         document.write('<br>'); //?
     }
 }
+
+function pyramidCustom(n){
+    for (var i = 1; i <= n; i++){ //?
+        var s = ""; //?
+        for(var j = 1; j <= (2*n-1); j++){ //?
+            (j >= n+1-i && j <= n-1+i ? s += "*" : s += " "); //?
+        }
+        console.log(s); //?
+    }
+}

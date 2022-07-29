d0f46043b2f4c765dcfcc6021bfebb881976b6c5
Mark <teamofmark@gmail.com>
2022-07-29 12:33:34 +0900

    toggleTodo

--- JQuery/js/3_addRemove.js
+++ JQuery/js/3_addRemove.js
@@ -20,4 +20,20 @@ function toggleCheck(){
 function toggleTodo(){
     // ? text: visibility -> 켜진눈 , visibility_off -> 꺼진눈.
     // todo. 켜진눈 -> input: text, 꺼진눈 -> input: password.
+    // * attr('name') -> 값 불러오기 / attr ('name','value') 값 바꿔치기
+    var $toggleTarget = $(".material-icons.toggleVisible");
+    var toggleStatus = false;
+    var $pwInput = $("#userPW");
+    
+    $toggleTarget.click(function(){
+        toggleStatus = !toggleStatus;
+        if(toggleStatus == true){
+            $toggleTarget.text("visibility");
+            $pwInput.attr("type","text");
+        }else{
+            $toggleTarget.text("visibility_off");
+            $pwInput.attr("type","password");
+        }
+    });
+
 }

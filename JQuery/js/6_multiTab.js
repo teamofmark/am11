938b4194827574cc04f3b60b17e300a1ce9d14aa
Mark <teamofmark@gmail.com>
2022-08-01 12:52:32 +0900

    6_한개의 함수로 복수제어 하기

---
+++ JQuery/js/6_multiTab.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    tabMenu("#tabMenuDepth0 li");
+    tabMenu("#tabMenuDepth1 li");
+});
+
+function tabMenu(tabNumb){
+    var $selectMenu = null;
+    
+    $(tabNumb).click(function(){
+        if($selectMenu != null){
+            $selectMenu.removeClass("activated");
+        }
+        $selectMenu = $(this);
+        $selectMenu.addClass("activated");
+    });
+}

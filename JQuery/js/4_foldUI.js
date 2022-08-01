e61a707107deba06f01d434000c47d0200c65b4a
Mark <teamofmark@gmail.com>
2022-08-01 11:59:05 +0900

    타일형식배치

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     imgAlignHori();
+
+    imgAlignMix();
 });
 
 // *case. 1 xAxis 배치.
@@ -18,5 +20,28 @@ function imgAlignHori(){
     });
     $(".reset").click(function(){
         img.css("left",0);
+    });
+}
+
+// *case. 2 x,yAxis 배치.
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%3)*imgHeight; //?
+            ypos = ; //todo. ypos 연산식 세우기
+
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            })
+        }
     });
 }

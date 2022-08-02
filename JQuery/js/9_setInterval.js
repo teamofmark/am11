bd2490b83fc0952407192c13c58486717992a951
Mark <teamofmark@gmail.com>
2022-08-02 12:57:09 +0900

    interval / clear 개념설명

---
+++ JQuery/js/9_setInterval.js
@@ -0,0 +1,34 @@
+var $circle = null;
+var railWidth = null;
+var runStep = 10;
+var timerID = 0;
+
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $circle = $("#circle");
+    railWidth = $("#rail").width();
+}
+function initEvent(){
+    $("#btn_start").click(moveStart);
+    $("#btn_stop").click(moveStop);
+}
+function moveStart(){
+    if(timerID == 0){
+        timerID = setInterval(moveCircle,0);
+    }
+}
+function moveCircle(){
+    var xpos = $circle.position().left;
+    xpos += runStep;
+    $circle.css("left",xpos);
+    if(xpos > railWidth || xpos < 0){
+        runStep *= -1;
+    }
+}
+function moveStop(){
+    clearInterval(timerID);
+    timerID = 0;
+}

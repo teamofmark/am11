b92f71f889ee8a49e7a0e5a863732c87a85f3fdf
Mark <teamofmark@gmail.com>
2022-08-03 13:02:26 +0900

    sort 기본개념 설명까지

--- JQuery/js/12_finJquery.js
+++ JQuery/js/12_finJquery.js
@@ -31,6 +31,7 @@ function initEvent(){
     $("#start").click(startGame);
 }
 
+// ?게임시작함수
 function startGame(){
     if(timerID == -1){
         timerID = setInterval(function(){
@@ -43,6 +44,7 @@ function startGame(){
 
 // ?물고기 이동함수
 function updateFishPosition(){
+    console.log("1. 물고기 이동 시작");
     for (var i = 0; i < $fishList.length; i++){
         var $fish = $fishList.eq(i); //? 현재 물고기
         var step = Math.ceil(Math.random()*10);//? 물고기 이동 범위.(10중 random, 올림한 정수값.)
@@ -52,4 +54,34 @@ function updateFishPosition(){
     }
 }
 
-// ?
+// ?물고기 위치 출력 함수
+function displayFishPositionInfo(){
+    console.log("2. 위치 출력 시작");
+    var info = "";
+    for (var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        info += i + "번 물고기 : " + $fish.position().left + "px <br>";
+    }
+    $info.html(info);
+}
+
+// ?결승선 통과판별 함수
+function checkGoalFish(){
+    var winnerList = [];
+
+    for (var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+                index: (i), position: fishCurrentPosition
+            });
+        }
+    } //? goalLine을 넘은 물고기가 한 마리라도 있다면 winnerList 배열에 position이라는 이름으로 저장 시작.
+
+    if(winnerList.length>0){ //? winnerList에 position값이 담기고 나면
+        winnerList.sort(function(a,b){
+            return b.position - a.position; //? position 값 기준 내림차순 정렬.
+        });
+    }
+}

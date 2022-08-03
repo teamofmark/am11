cf284b26d74cbacc851d424ee9494166c4bf73cf
Mark <teamofmark@gmail.com>
2022-08-03 12:36:39 +0900

    물고기 이동함수 완료

--- JQuery/js/12_finJquery.js
+++ JQuery/js/12_finJquery.js
@@ -7,6 +7,7 @@ var $info = null;
 $(document).ready(function(){
     init();
     fishStartPosition();
+    initEvent();
 });
 
 function init(){
@@ -25,3 +26,30 @@ function fishStartPosition(){
         });
     }
 }
+
+function initEvent(){
+    $("#start").click(startGame);
+}
+
+function startGame(){
+    if(timerID == -1){
+        timerID = setInterval(function(){
+           updateFishPosition(); // ? 물고기 이동함수
+           displayFishPositionInfo(); // ? 이동수치 출력함수
+           checkGoalFish(); // ? 결승선 통과판별 함수
+        }, 200);
+    }
+}
+
+// ?물고기 이동함수
+function updateFishPosition(){
+    for (var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i); //? 현재 물고기
+        var step = Math.ceil(Math.random()*10);//? 물고기 이동 범위.(10중 random, 올림한 정수값.)
+        var newLeft = $fish.position().left + step;
+
+        $fish.css("left", newLeft);
+    }
+}
+
+// ?

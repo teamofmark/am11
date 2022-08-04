16392fe3c9abd58c31fd6d8e3a76f6c2bccee588
Mark <teamofmark@gmail.com>
2022-08-04 12:19:09 +0900

    css customize

--- plugin/js/custom.js
+++ plugin/js/custom.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
         mode: 'horizontal', //? default : horizontal(수평방향전환), vertical(수직방향전환), fade(사라지고나타나고)
         speed: 750, //? default : 500. 전환속도 제어
         ticker: false, //? default :  false. slider를 controler통하지 않은 상태로 흘러가기. speed,mode 영향받음.
-        tickerHover: false//? default : false. ticker 상태일 때 onMouseOver로 중지시키기.
+        tickerHover: false,//? default : false. ticker 상태일 때 onMouseOver로 중지시키기.
     
-    
+        // *pager - indicator
+        pager: true, //? default: true. pager 켜기/끄기
+        pagerType: 'full', //? default: full.(circle). short: count로 변경
+        //? pagerShortSeparator: ' - ' default: / -> . 식별자변경.
     });
 });

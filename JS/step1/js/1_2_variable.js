442145293c1777ce6dd4205d5c991ce2b7b10cd8
Mark <teamofmark@gmail.com>
2022-11-11 12:53:54 +0900

    변수선언및초기화 관련\

--- JS/step1/js/1_2_variable.js
+++ JS/step1/js/1_2_variable.js
@@ -58,3 +58,11 @@
     var objClass = new Markclass();
     objClass.showName();
 */
+
+// *case. 1 변수 생성
+var name = "Mark", age = 42, gender = man; //? 변수 선언과 동시 초기화.
+var radius, pi; //? 변수 선언. (한번에)
+radius = 10, pi = 3.14159265; //? 변수 초기화(한번에)
+
+
+// ? ES6 - let, const... 선언하는 경우. (퍼블리셔는 몰라도됨)

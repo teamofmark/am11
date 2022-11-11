6c7368c70e08c4fb0dffa40afa6343689e5b562d
Mark <teamofmark@gmail.com>
2022-11-11 12:33:43 +0900

    debug 수동으로 하는법

---
+++ JS/step1/js/1_1_check.js
@@ -0,0 +1,10 @@
+// ? programming의 기본 - 선 / 처 / 출.
+function runAlert(){
+    alert("hello, js");
+}
+function runWrite(){
+    document.write("hello" + "<br>" + "js");
+}
+function runLog(){
+    console.log("hello \n js");
+}

28ff42397695605273b517e8fbb58ca324e39ca0
Mark <teamofmark@gmail.com>
2022-11-14 12:54:47 +0900

    숫자형변환 한번 해보기

---
+++ JS/step1/js/1_4_typeofChange.js
@@ -0,0 +1,20 @@
+/*
+    ? 형변환
+    * JS - "10" + 10 = 1010 / 사람 - "10" + "10" = 20 - 이런 인식차를 해결하기위함.
+    ! 암시적형변환 - js Engine이 자동으로 변환.  /  명시적 형변환 - 개발자가 직접 지정 변환.
+
+    ? 암시적 형변환 CASE
+    * 1. 숫자 + 문자 = 문자.
+    * 2. 불린 + 숫자 = 숫자.
+    * 3. 불린 + 문자 = 문자.
+    
+    ? 명시적 형변환
+    * method 나 lib Core func를 이용하여 강제로 숫자 -> 문자 로 변환.
+*/
+// *case.1 자신의 나이를 입력받고 20을 더한 값 출력.
+function agePlus(){
+    var age = window.prompt('나이입력');
+    var result = Number(age) + 20; //? Number - 전역함수(객체)는 정,실수 무관 무조건 숫자형변환
+    console.log(typeof(age));
+    console.log(result);
+}

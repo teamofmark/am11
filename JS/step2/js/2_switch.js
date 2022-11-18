96b50bdf18662eae15c156e242ec4801b045cbc1
Mark <teamofmark@gmail.com>
2022-11-18 11:30:45 +0900

    기본문법설명

--- JS/step2/js/2_switch.js
+++ JS/step2/js/2_switch.js
@@ -1,6 +1,22 @@
 
 /*
 ?switch
-switch는 조건식의 결과가 case에 걸리는 경우, 비교할때 사용한다.
+* switch는 조건식의 결과가 case에 걸리는 경우, 비교할때 사용한다.
 ( >,<,>=,<=,!= 와 같은 연산자를 사용 안했을 경우에만)
 */
+// *case.1 기본문법
+function switchGrammar(){
+    var input = Number(prompt('숫자를 입력하라', '숫자'));
+    
+    switch(input % 2){
+        case 0:
+            alert('짝수입니다.');
+            break;
+        case 1:
+            alert('홀수입니다.');
+            break;
+        default:
+            alert('숫자가아닙니다.');
+            break;
+    }
+}

2274f0c9304e7d887483c3b63adea771892e080b
Mark <teamofmark@gmail.com>
2022-11-18 11:48:18 +0900

    switch 실습

--- JS/step2/js/2_switch.js
+++ JS/step2/js/2_switch.js
@@ -20,3 +20,41 @@ function switchGrammar(){
             break;
     }
 }
+// *case. 2 lotto 과제를 switch로 변경
+function lotto(){
+    var numbValue = window.prompt('입력하실 번호는?');
+    switch(numbValue){
+        case "1":
+            alert("1등 10억");
+            break;
+        case "2":
+            alert("2등 5천만원");
+            break;
+        case "3":
+            alert("3등 300만원");
+            break;
+        default:
+            alert("낙첨. ㅂㅇ");
+            break;
+    }
+}
+// todo. switch 문을 이용한 계산기
+// ? 첫번째 숫자와 연산자와 두번째 숫자를 입력받아 계산해주는 계산기.
+function calculator(){
+    var firstNumb = window.prompt("첫번째 숫자를 입력하세요.");
+    var operator = window.prompt("사칙연산자중 하나를 입력하세요.(+,-,*,/)");
+    var lastNumb = window.prompt("두번째 숫자를 입력하세요.");
+    
+    firstNumb = parseInt(firstNumb);
+    lastNumb = parseInt(lastNumb);
+
+    switch(operator){
+        case "+":
+            alert();// todo. 더하기 연산식
+            break;
+            // todo.  -
+            // todo.  *
+            // todo.  /
+            // todo. default
+    }
+}

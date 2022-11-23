9c6ac787bc258f47f7ffbba2ca00271bce7fe74c
Mark <teamofmark@gmail.com>
2022-11-23 12:24:19 +0900

    return 주석부 배포

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -34,4 +34,46 @@ function sumParam(num1,num2){
     * arguments를 이용해서 함수내 인수들을 참조 할 수 있고 이 객체의 함수에 전달된 각 인수항목을 포함한다.
     * 단, arguments는 array와 비슷해보이지만 length를 구하는 방식을 제외하고는 array와는 다르다.
     * 필요에 따라 array로 추가 변환하여 사용 할 수 있다.
+
+    ? ex> 회원정보를 당길건데 (backEnder = O, frontEnder = X) id를 당기면 다온다.
+    ? id를 당겼을 때 ? 몇개? 뭐가?...
+*/
+function sumAll(){
+    var willReturn = 0;
+    for(var i = 0; i < arguments.length; i++){
+        willReturn += arguments[i];
+    }
+    console.log(willReturn);
+}
+// !tune.
+function sumAllEX(){
+    var willReturn = 0;
+    for (var i in arguments){
+        willReturn += arguments[i];
+    }
+    return willReturn;
+}
+
+// *case. 5 "자신의 이름"의 나이는 "자신의 나이" 세 입니다. 를 arguments를 활용해 만들어보기.
+function showInfo(){
+    console.log("0 = " + arguments[0]);
+    console.log("1 = " + arguments[1]);
+    alert(arguments[0] + "의 나이는" + arguments[1] + "세 입니다.");
+}
+/*
+    ?return (리턴)
+    * 함수 내부는 함수라는 범위 안에 갇히기 때문에, 한번 실행되면 함수 외부에서 접근 불가.
+    * 이 때 매개변수를 활용하면 함수 내부로 data를 전달 할 수 있는데, return value는 이와 반대 되는 값.
+    * 즉, 매개변수 값이 함수 외부에서 내부로 들어오는 입력 부라면, 리턴값은 함수 내부에서 처리한 결과를
+    * 함수 외부로 전달하기 위해 사용하는 출력 부.
+
+
+    !ex.1 함수f(x)안에 넣는 값이 매개변수, 결과로 나오는 x*x 가 리턴값.
+        function f(x) { return x * x; }
+        alert(f(3));
+
+    !ex.2 alert에서 매개변수를 두개를 선언하면 추가된 매개변수를 무시한다.
+    //*(매개변수가 초과 됐을 시) 또한 원래 함수의 매개변수보다 적게 입력하면 undefined를 출력한다.
+        alert('기존매개변수', '추가매개변수');
+        prompt('기존매개변수');
 */

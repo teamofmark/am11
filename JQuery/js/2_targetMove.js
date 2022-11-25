af7e62fa89fb8d69400837e26f59a0c48180a026
Mark <teamofmark@gmail.com>
2022-11-25 12:57:59 +0900

    치면서 js Engine처럼 생각해보기

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -17,3 +17,21 @@ function moveCircle(){
     }
     // ? 처리부
 }
+// *exp.2
+function detailMoveCircle(){
+    var $circle = $(".circleDetail");
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+
+    if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+        alert('잘못된 수치입니다. 0 ~ 380이내로 입력하세요.');
+    }else{
+        $circle.css({
+            "left" : xpos,
+            "top" : ypos
+        });
+    }
+}

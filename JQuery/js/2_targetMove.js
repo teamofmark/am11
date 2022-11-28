b1f08abfc9b12d2f9dd1d3fce8fbc4a97c59527a
Mark <teamofmark@gmail.com>
2022-11-28 12:37:06 +0900

    문제 풀이

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -91,7 +91,18 @@ function keyControl(){
                 currentXpos -= range;
                 break;
         }
-
+        if(currentXpos < 0){
+            currentXpos = 0;
+        }
+        if(currentYpos < 0){
+            currentYpos = 0;
+        }
+        if(currentXpos > 380){
+            currentXpos = 380;
+        }
+        if(currentYpos > 380){
+            currentYpos = 380;
+        }
         $circle.css("left",currentXpos);
         $circle.css("top",currentYpos);
 

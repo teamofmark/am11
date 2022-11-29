27bb1c1d2307c0ab1c6829c727d5380b102d3574
Mark <teamofmark@gmail.com>
2022-11-29 11:21:43 +0900

    toggle 2번째 예제

--- JQuery/js/3_addRemove.js
+++ JQuery/js/3_addRemove.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     toggleCheck();
+    toggleTodo();
 });
 function toggleCheck(){
     var $checkTarget = $(".material-icons.checkIco");
@@ -12,6 +13,22 @@ function toggleCheck(){
             $checkTarget.text("check_box");
         }else{
             $checkTarget.text("check_box_outline_blank");
+        }
+    });
+}
+
+function toggleTodo(){
+    var $toggleTarget = $(".material-icons.toggleVisible");
+    var toggleStatus = false;
+
+    $toggleTarget.click(function(){
+        toggleStatus = !toggleStatus;
+        if(toggleStatus == true){
+            $toggleTarget.text("visibility");
+            $("#userPW").attr("type","text");
+        }else{
+            $toggleTarget.text("visibility_off");
+            $("#userPW").attr("type","password");
         }
     });
 }

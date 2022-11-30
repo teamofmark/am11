0e35a97b5f0afcd7834e8f720ae36503c296fce9
Mark <teamofmark@gmail.com>
2022-11-30 11:25:08 +0900

    과제 문제 배포

---
+++ JQuery/js/5_foldTODO.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    gallery();// ? 초기배치.
+    // ?n열 버튼 눌렀을때 (열 매개변수,너비,높이);
+    // ! 너비 높이는 1열에 들어가는 수치가 기준이 됨.
+});
+
+function gallery(){
+    // ? 기존에 풀었던 문제들처럼
+    // ? image's 선언, length(forfor),xpos,ypos.
+    // ? for loop 잡아서 알아서 작동될수 있는 logic.
+    // ? css 적용.(기존에 했던것 left, top 말고도 width,height)
+}

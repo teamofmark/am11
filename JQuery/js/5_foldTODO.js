c7520c1255ff28455da2c578169d815aef47a31f
Mark <teamofmark@gmail.com>
2022-12-01 11:36:31 +0900

    foldTODO 풀이

--- JQuery/js/5_foldTODO.js
+++ JQuery/js/5_foldTODO.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
-    gallery();// ? 초기배치.
-    // ?n열 버튼 눌렀을때 (열 매개변수,너비,높이);
-    // ! 너비 높이는 1열에 들어가는 수치가 기준이 됨.
+    gallery(0,200,200);
+    $(".1way").click(function(){gallery(1,1000,1000)});
+    $(".2way").click(function(){gallery(2,500,500)});
+    $(".3way").click(function(){gallery(3,333,333)});
+    $(".4way").click(function(){gallery(4,250,250)});
+    $(".5way").click(function(){gallery(5,200,200)});
+    $(".6way").click(function(){gallery(6,166,166)});
+    $(".7way").click(function(){gallery(7,142,142)});
+    $(".8way").click(function(){gallery(8,125,125)});
+    $(".9way").click(function(){gallery(9,111,111)});
+    $(".10way").click(function(){gallery(10,100,100)});
 });
 
-function gallery(){
-    // ? 기존에 풀었던 문제들처럼
-    // ? image's 선언, length(forfor),xpos,ypos.
-    // ? for loop 잡아서 알아서 작동될수 있는 logic.
-    // ? css 적용.(기존에 했던것 left, top 말고도 width,height)
+function gallery(count, imgWidth, imgHeight){
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+
+    var xpos, ypos;
+
+    for(i = 0; i<$imgLength; i++){
+        var $image = $images.eq(i); //? eq = DOM 요소 세트를 나타내는 jQuery 객체가 주어지면 .eq()메소드는 해당 세트 내의 한 요소에서 새 jQuery 객체를 구성한다.
+        
+        xpos = (i % count) * imgWidth;
+        ypos = parseInt(i / count) * imgHeight;
+
+        $image.css({
+            'left' : xpos,
+            'top' : ypos,
+            'width' : imgWidth,
+            'height' : imgHeight
+        })
+    }
 }

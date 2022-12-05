357a8b5f17f7eeeb32edfdb06d18274f32862ef1
Mark <teamofmark@gmail.com>
2022-12-05 12:20:30 +0900

    나머지 잔여속성 일괄

--- plugin/bxSlider/js/customize.bxslider.js
+++ plugin/bxSlider/js/customize.bxslider.js
@@ -26,8 +26,8 @@ $(document).ready(function(){
         pager: true, //? default: true. pager 켜기/끄기
         pagerType: 'full', //? default: full.(circle). short: count로 변경
         //? pagerShortSeparator: ' - ' default: / -> . 식별자변경.
-        //? pagerSelector: '.newPager', default: ''. 공간 생성 및 class 부여 하면 pager 뼈대제공.
-        pagerCustom: '.customPager',
+        pagerSelector: '.newPager', // ? default: ''. 공간 생성 및 class 부여 하면 pager 뼈대제공.
+        // pagerCustom: '.customPager',
 
         // *controler
         controls: true,
@@ -54,8 +54,33 @@ $(document).ready(function(){
         swipeThreshold: 50, //? default: 50. ? slide 터치스와이프시. 터치상태로 움직여야하는 px수.
         oneToOneTouch: false,  //? default: true. hori,verti 일경우 터치상태를 slide가 따라다닌다.
         preventDefaultSwipeX: true, //? default: true. screen자체 터치상태에서 x축 움직임을 중지 
-        preventDefaultSwipeY: false //? default: false. screen자체 터치상태에서 Y축 움직임을 중지 
+        preventDefaultSwipeY: false, //? default: false. screen자체 터치상태에서 Y축 움직임을 중지 
     
         // ! carousel
+        minSlides: 1, //? default: 1. 최소 노출 슬라이드 장수.
+        maxSlides: 3, //? default: 1. 최대 노출 슬라이드 장수.
+        slideWidth: 700, //? default: 0. 슬라이드 너비.
+        //? slideMargin: 100  slide간 간격 설정.(다수의 slide(carousel)를 사용시에 설정필요.)
+        //? shrinkItems: default: false. max/min 기준으로 viewport에 맞게 img를 축소.
+         //? moveSlides: 2 default: 0. 전환시 이동할 슬라이드 장수.
+         
+        // *비중요
+        wrapperClass: 'bx-wrapper', //? default: 'bx-wrapper' 종속된 css 를 해제하고 다른 부모 class로 변경한다.
+        preloadImages: 'visible', //? default: visible. slider시작전에 한장(첫장)만 load, all. slider 시작전에 slide모든 image download.
+        useCSS: true, //? default: true. css animation(하드웨어가속)사용. 켜기(권장) 끄기.
+        //? slideSelector : $(parent child) //? default: ''. slide로 작동될 요소를 직접선택 가능.
+        //? keyboardEnabled: default: false. slider keyboard 조작가능 하게 변경.
+        // ? autoDirection: 'next', 자동전환 방향설정 'prev'는 일반적이지 않다.
+        
+        // *Public methods.
+        // ? goToslide : 특정 index 슬라이드로 전환.
+        // ? goToNextSlide : '다음' 슬라이드로 전환. (ex> wheelScroll에 반응하여 다음,이전 작동..)
+        // ? goToPrevSlide : '이전' 슬라이드로 전환. 
+        // ! startAuto, stopAuto: 기존 auto로 충분히 대체가 가능.
+        // ? getCurrentSlide : 현재 슬라이드 찾기(반환).
+        // ? getSlideCount : 슬라이드.length 찾기(반환).
+        // ? redrawSlider : slider . show / hide or display none / block 등의 숨김해제 이후 재생성 할때.
+        // ? reloadSlider : slider를 다시 불러오기. jquery mediaQuery. (ex> 장치변환시 slider 다시부르기.)
+        // ? destroySlider : slider 없애기. (slider 구조도 사라짐.) 
     });
 });

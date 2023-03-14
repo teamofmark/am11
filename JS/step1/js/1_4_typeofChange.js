31a2f80baaddbf9210a88abd7965562ee158eefd
Mark <teamofmark@gmail.com>
2023-03-14 12:06:53 +0900

    형변환하기 완료

--- JS/step1/js/1_4_typeofChange.js
+++ JS/step1/js/1_4_typeofChange.js
@@ -70,19 +70,19 @@ function numbString3(){
 // todo. 1-1 암시적형변환
 function todo1(){
     var test = 100;
-    var result = test /* 형변환 코드 */;
+    var result = test + "" /* 형변환 코드 */;
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 1-2 String 형변환
-function todo1(){
+function todo2(){
     var test = 100;
-    var result = test /* 형변환 코드 */;
+    var result = String(test) /* 형변환 코드 */;
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 1-3 toString형변환
-function todo1(){
+function todo3(){
     var test = 100;
-    var result = test /* 형변환 코드 */;
+    var result = test.toString() /* 형변환 코드 */;
     console.log(typeof(result) + ' = ' + result);
 }
 
@@ -91,12 +91,12 @@ function todo1(){
 // todo. 2-1 parse 형변환
 function todo4(){
     var test = "100.51";
-    var result = test + 150 + "20"; // todo. 형변환.
+    var result = parseFloat(test) + 150 + parseInt("20"); // todo. 형변환.
     console.log(typeof(result) + ' = ' + result);
 }
 // todo. 2-1 Number 형변환
 function todo5(){
     var test = "100.51";
-    var result = test + 150 + "20"; // todo. 형변환.
+    var result = Number(test) + 150 + Number("20"); // todo. 형변환.
     console.log(typeof(result) + ' = ' + result);
 }

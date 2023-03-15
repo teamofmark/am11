8035d483364f518a78423f19bb11c35569af630d
Mark <teamofmark@gmail.com>
2023-03-15 11:47:18 +0900

    연산자 기초 종료

--- JS/step1/js/1_5_operator.js
+++ JS/step1/js/1_5_operator.js
@@ -106,3 +106,48 @@ function numberPlusTodo(){
     ++number;
     console.log(number); //? 
 }
+
+// *case. 6 숫자 10이 담긴 변수a 와 숫자 20이 담긴 변수b의 크기를 비교하여 결과출력.
+function compareOperator1(){
+    var a = 10;
+    var b = 20;
+    var result = a > b;
+    console.log("10 > 20 = ?" + result);
+}
+
+// *case. 7 숫자 10이 담긴 변수a 와 숫자 20이 담긴 변수b가 같은지 비교하여 결과출력.
+function compareOperator2(){
+    var a = 10;
+    var b = 20;
+    var result = a==b;
+    console.log("10 = 20 ? " + result);
+}
+// todo. 숫자 10이 담긴 변수a 와 숫자 20이 담긴 변수b가 다른지를 비교하여 결과출력.
+function compareOperator3(){
+    var a = 10;
+    var b = 20;
+    var result = a!=b;
+    console.log(" 10 != 20 ?" + result);
+}
+// *case.8 a변수에는 '여자', b변수에는 '웹디자이너'라는 값이 저장 되어있다. 담긴 두값 모두 맞는지 비교출력.
+function logicalOperator1(){
+    var a = "여자";
+    var b = "웹디자이너";
+    var result = (a =="여자")&&(b == "웹디자이너");
+    console.log("result=" + result);
+}
+// *case.9 a변수에는 '남자', b변수에는 '웹퍼블리셔' 라는 값이 저장되어 있다. 담긴 두값 중 하나라도 맞는지 비교출력.
+function logicalOperator2(){
+    var a = "남자";
+    var b = "웹디자이너";
+    var result = (a == "남자")||(b == "웹퍼블리셔");
+    console.log("result = " + result);
+}
+// todo. 다음 코드를 작성 후 결과를 예상한 다음 출력하라.
+function compareOperator4(){
+    console.log(30 > 20 > 10); //? 결과 : ?. -> 적고 실행. -> true > 10 ? err. / 1 > 10 -> false.
+}
+// todo. 만약 본인이 예상한 답이 아니라면. 정상적인 결과가 나올 수 있도록 수정.
+function compareOperator4R(){
+    console.log(30 > 20 && 20 > 10); // todo. 수정하기.
+}

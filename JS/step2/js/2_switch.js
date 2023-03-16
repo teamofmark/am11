db3372f25aceb3e54972871a4d1e181d4b044adb
Mark <teamofmark@gmail.com>
2023-03-16 12:48:00 +0900

    switch 진행전

--- JS/step2/js/2_switch.js
+++ JS/step2/js/2_switch.js
@@ -4,3 +4,14 @@
     ( >,<,>=,<=,!= 와 같은 연산자를 사용 안했을 경우에만)
 */
 // *case. 1 문법
+function switchGrammar(){
+    var input = Number(prompt('숫자를 입력하라.'));
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}

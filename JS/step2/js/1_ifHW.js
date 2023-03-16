7710356cc5e6f77be969e8d46c5a67daa8baa7ac
Mark <teamofmark@gmail.com>
2023-03-16 12:50:38 +0900

    조건문과제 배포

--- JS/step2/js/1_ifHW.js
+++ JS/step2/js/1_ifHW.js
@@ -1,10 +1,10 @@
-//todo hw.1 각 수를 입력 받아 해당하는 조건의 메시지를 출력하라.
+//todo. hw.1 각 수를 입력 받아 해당하는 조건의 메시지를 출력하라.
 //? 1일 경우 "1등 10억", 2일 경우 "2등 5천만원", 3일 경우 "3등 300만원", 그외 수일 경우 "낙첨되었습니다."
 
-//todo hw.2 다음 코드를 간소화 시키시오.
+//todo. hw.2 다음 코드를 간소화 시키시오.
 function btnMouseEx(){
     var clickBtn = window.prompt("누르실 마우스 버튼을 입력하세요 (왼쪽,오른쪽,가운데)");
-
+    // ! if x 1개 else 로 처리.
     if(clickBtn=="오른쪽"){
         document.write("오른쪽!");
     }else if(clickBtn=="왼쪽"){

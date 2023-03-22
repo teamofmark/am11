a6063ec51ed25e8b91b6d49f48e4186be4f1ace1
Mark <teamofmark@gmail.com>
2023-03-22 12:36:14 +0900

    다음 실습문제

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -102,3 +102,19 @@ function arraySum(){
     }
     document.write('해당 배열의 총합은 = ' + result);
 }
+
+// todo. 다음실행구문으로 전달받은 매개변수로 계산하여 결과를 출력하는 함수 만들기.
+/*
+    //* 실행구문
+    document.write("1 결과 =" + calculator("+", 20, 10));
+    document.write("2 결과 =" + calculator("-", 20, 10));
+    document.write("3 결과 =" + calculator("*", 20, 10));
+    document.write("4 결과 =" + calculator("/", 20, 10));
+    document.write("5 결과 =" + calculator("%", 20, 10));
+    //* 결과 
+    결과 = 30
+    결과 = 10
+    결과 = 200
+    결과 = 2
+    결과 = 잘못된 연산자 입니다.
+*/

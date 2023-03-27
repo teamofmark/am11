00ff62cf2a21f64ca4ca588c2de48aaa2a36bd8b
Mark <teamofmark@gmail.com>
2023-03-27 12:41:05 +0900

    attr 함수 사용해보기

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -20,6 +20,21 @@ function toggleCheck(){
 
 // todo. passwordComponent 만들어보기
 function toggleTodo(){
+    var $toggleTarget = $(".material-icons.toggleVisible");
+    var $toggleInput = $("#userPW");
+    var toggleStatus = false;
+
+    $toggleTarget.click(function(){
+        toggleStatus = !toggleStatus;
+        if(toggleStatus == true){
+            $toggleTarget.text('visibility');
+            $toggleInput.attr('type','text');
+        }else{
+            $toggleTarget.text('visibility_off');
+            $toggleInput.attr('type','password');
+        }
+    }); 
+
     // todo. $target.attr('attr이름') -> target의 'attr이름'의 값을 가져와라.
     // todo. $target.attr('attr이름','attr속성값') -> target의 'attr이름'의 값을 'attr속성값'으로 바꿔라.
 }

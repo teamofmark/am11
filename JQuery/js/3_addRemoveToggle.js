d40ad4fe0820923143f5c979a18306b34462387b
Mark <teamofmark@gmail.com>
2023-03-27 12:15:41 +0900

    toggle 개념이해

---
+++ JQuery/js/3_addRemoveToggle.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    toggleCheck();
+});
+function toggleCheck(){
+    var $checkTarget = $(".material-icons.checkIco");
+    var $checkLabel = $(".label.checkLabel");
+    var checkStatus = false;
+
+    $checkLabel.click(function(){
+        checkStatus = !checkStatus;
+        console.log(checkStatus);
+        if(checkStatus == true){
+            $checkTarget.text("check_box");
+        }else{
+            $checkTarget.text("check_box_outline_blank");
+        }
+    });
+}

1002b985e03445bd5053d466337364e8a2ba74ab
Mark <teamofmark@gmail.com>
2023-03-27 12:20:08 +0900

    attr 함수 사용법

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     toggleCheck();
+    toggleTodo();
 });
 function toggleCheck(){
     var $checkTarget = $(".material-icons.checkIco");
@@ -8,11 +9,17 @@ function toggleCheck(){
 
     $checkLabel.click(function(){
         checkStatus = !checkStatus;
-        console.log(checkStatus);
+        // console.log(checkStatus);
         if(checkStatus == true){
             $checkTarget.text("check_box");
         }else{
             $checkTarget.text("check_box_outline_blank");
         }
     });
+}
+
+// todo. passwordComponent 만들어보기
+function toggleTodo(){
+    // todo. $target.attr('attr이름') -> target의 'attr이름'의 값을 가져와라.
+    // todo. $target.attr('attr이름','attr속성값') -> target의 'attr이름'의 값을 'attr속성값'으로 바꿔라.
 }

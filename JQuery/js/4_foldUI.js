3870f2ab54b335c59fecefa51e89fe8067192686
Mark <teamofmark@gmail.com>
2023-03-28 12:24:58 +0900

    foldUI

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -19,5 +19,28 @@ function imgAlignHori(){
     })
 }
 function imgAlignMix(){
-    
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%3)*imgHeight; // 0,1,2,0,1,2,0,1,2....
+            ypos = parseInt(i/3)*imgHeight;
+            image.css({
+                "left": xpos,
+                "top" : ypos
+            }); 
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        });
+    });
 }

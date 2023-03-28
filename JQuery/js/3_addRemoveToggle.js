1feae2a950884c79baa18ceb970ad35719dcf110
Mark <teamofmark@gmail.com>
2023-03-28 11:10:13 +0900

    tabUI 완성

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -42,7 +42,10 @@ function toggleTodo(){
 
 function tabUI(){
     $(".tabMenu li").click(function(){
+        var activeTab = $(this).attr("data-tabNumb");
         $(".tabMenu li").removeClass("activated");
         $(this).addClass("activated");
+        $(".tabPage").removeClass("activated");
+        $("#" + activeTab).addClass("activated");
     });
 }

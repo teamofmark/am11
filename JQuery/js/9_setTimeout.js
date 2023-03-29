2f5f5f070c1956d4bf8be9bf18ec10f09dfd8600
Mark <teamofmark@gmail.com>
2023-03-29 12:42:22 +0900

    todo / 재시작 기능 만들어보기

--- JQuery/js/9_setTimeout.js
+++ JQuery/js/9_setTimeout.js
@@ -7,6 +7,7 @@ var $resetBtn = null;
 $(document).ready(function(){
     init();
     scoreCount(count,$score,$circle,playState);
+    $resetBtn.click(restart);
 });
 function init(){
     $score = $("#score");
@@ -24,4 +25,10 @@ function scoreCount(count,$score,$circle,playState){
         alert("게임종료!");
         playState=false;
     },5000);
+}
+function restart(){
+    playState = true;
+    count = 0;
+    $score.text(count + '점');
+    scoreCount(count,$score,$circle,playState);
 }

f2ec60c5ff15e9bf2fab6b031b04a2a55d34710b
Mark <teamofmark@gmail.com>
2023-03-29 11:27:09 +0900

    closure 설명

--- JQuery/js/7_multiTab.js
+++ JQuery/js/7_multiTab.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 function tabMenu(tabNumb){
     var $selectMenu = null;
     $(tabNumb).click(function(){
+        // ? closureEffect (function) - 내부에서 selectMenu라는 외부 변수 사용중.
         if($selectMenu != null){
             $selectMenu.removeClass("activated");
         }
@@ -13,3 +14,18 @@ function tabMenu(tabNumb){
         $selectMenu.addClass("activated");
     });
 }
+
+/*
+var $selectMenu = null;
+
+function tabMenu(tabNumb){
+    $(tabNumb).click(tabEvent);
+}
+function tabEvent(){
+    if($selectMenu != null){
+        $selectMenu.removeClass("activated");
+    }
+    $selectMenu = $(this);
+    $selectMenu.addClass("activated");
+}
+*/

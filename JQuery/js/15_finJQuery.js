60dc4d69c5bffc05448e4018da1ad18a54236e5a
Mark <teamofmark@gmail.com>
2023-03-31 12:52:49 +0900

    물고기 초기배치까지

---
+++ JQuery/js/15_finJQuery.js
@@ -0,0 +1,25 @@
+var $fishList = null;
+var timerID = -1;
+var $info = null;
+var goalLine = 0;
+var $fishWidth = 0;
+
+$(document).ready(function(){
+    init();
+    fishStartPosition();
+});
+function init(){
+    $fishList = $(".fish");
+    $info = $("#info");
+    $fishWidth = $(".fish img").width();
+    goalLine = $("#bar").position().left - $fishWidth;
+}
+function fishStartPosition(){
+    for (var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        $fish.css({
+            left: 0,
+            top: 100 + (i * 150)
+        });
+    }
+}

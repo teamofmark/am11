9b5f505538345f041c8c093f5f612136f28bc924
Mark <teamofmark@gmail.com>
2023-04-03 12:38:27 +0900

    jquery 최종예제 풀이완료

--- JQuery/js/15_finJQuery.js
+++ JQuery/js/15_finJQuery.js
@@ -57,3 +57,47 @@ function displayFishPositionInfo(){
     }
     $info.html(info);
 }
+
+function checkGoalFish(){
+    console.log('3. 결승선 통과 감지중');
+    var winnerList = [];
+    for(var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+                index: (i), position: fishCurrentPosition
+            });
+        }
+    } //? 0~2번물고기의 현재 위치정보를 0,1,2,번 index에 position이라는 이름으로 저장.
+    
+    if(winnerList.length > 0){
+        winnerList.sort(function(a,b){
+            return b.position - a.position;
+        }); //? compareFunction - 비교함수를 매개변수로 삽입하여 사용.
+        /*
+            ! data.sort([compareFunction]);
+            ! 매개변수 = compareFunction - 정렬순서 정의 함수. 생략시 오름 차순 처리. (정순)
+            ? (문자열변환 후 유니코드값의 크기에 따라 정렬)
+            ! 리턴값 = 정렬이 완료된 결과 값.
+            ? 리턴값 < 0 : a가 b보다 앞에
+            ? 리턴값 = 0 : a와 b순서를 바꾸지 않음.
+            ? 리턴 값 > 0 : b가 a보다 앞에
+
+            * 한글정순 -> target.sort();
+            * 한글역순
+            * sort(function(a,b){
+            *   var a = a.toString().toLowerCase();
+            *   var b = b.toString().toLowerCase();
+            *   return (b < a) ? -1 : (b == a) ? 0 : 1;
+            * });
+        */
+        
+        endGame(); //? 게임종료 함수
+        alert('vict! ' + winnerList[0].index + 'th Fish!');
+    }
+}
+function endGame(){
+    console.log('4. 결승선 도달. 게임종료.');
+    clearInterval(timerID);
+    timerID = -1;
+}

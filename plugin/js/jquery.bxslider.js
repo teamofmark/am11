8fd85e752dfb27db2481dde8fedab3fd6f3db2e5
Mark <teamofmark@gmail.com>
2023-04-06 12:00:11 +0900

    bxslider Fix

--- plugin/js/jquery.bxslider.js
+++ plugin/js/jquery.bxslider.js
@@ -5,7 +5,7 @@
  * Licensed under MIT (http://opensource.org/licenses/MIT)
  */
 
-;(function($) {
+ ;(function($) {
 
   var defaults = {
 
@@ -368,7 +368,7 @@
           // add the current slide to the children
           children = slider.children.eq(currentIndex);
           // cycle through the remaining "showing" slides
-          for (i = 1; i <= slider.settings.maxSlides - 1; i++) {
+          for (var i = 1; i <= slider.settings.maxSlides - 1; i++) {
             // if looped back to the start
             if (currentIndex + i >= slider.children.length) {
               children = children.add(slider.children.eq(i - 1));
@@ -1106,7 +1106,7 @@
       if (e.type !== 'touchstart' && e.button !== 0) {
         return;
       }
-      e.preventDefault();
+      // e.preventDefault();
       //disable slider controls while user is interacting with slides to avoid slider freeze that happens on touch devices when a slide swipe happens immediately after interacting with slider controls
       slider.controls.el.addClass('disabled');
 
@@ -1117,12 +1117,12 @@
         slider.touch.originalPos = el.position();
         var orig = e.originalEvent,
         touchPoints = (typeof orig.changedTouches !== 'undefined') ? orig.changedTouches : [orig];
-		var chromePointerEvents = typeof PointerEvent === 'function'; 
-		if (chromePointerEvents) { 
-			if (orig.pointerId === undefined) { 
-				return;
-			} 
-		}
+        var chromePointerEvents = typeof PointerEvent === 'function'; 
+        if (chromePointerEvents) { 
+          if (orig.pointerId === undefined) { 
+            return;
+          } 
+        }
         // record the starting touch x, y coordinates
         slider.touch.start.x = touchPoints[0].pageX;
         slider.touch.start.y = touchPoints[0].pageY;
@@ -1187,13 +1187,19 @@
 
       // x axis swipe
       if ((xMovement * 3) > yMovement && slider.settings.preventDefaultSwipeX) {
-        e.preventDefault();
-      // y axis swipe
+        if(e.hasOwnProperty('cancelable') && e.cancelable) {
+          e.preventDefault();
+        }
+        // y axis swipe
       } else if ((yMovement * 3) > xMovement && slider.settings.preventDefaultSwipeY) {
-        e.preventDefault();
+        if(e.hasOwnProperty('cancelable') && e.cancelable) {
+          e.preventDefault();
+        }
       }
       if (e.type !== 'touchmove') {
-        e.preventDefault();
+        if(e.hasOwnProperty('cancelable') && e.cancelable) {
+          e.preventDefault();
+        }
       }
 
       if (slider.settings.mode !== 'fade' && slider.settings.oneToOneTouch) {

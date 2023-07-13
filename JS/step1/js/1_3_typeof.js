dce8728c472f12ff412f0d80e2c6687c1bd1e857
Mark <teamofmark@gmail.com>
2023-07-13 11:45:16 +0900

    실습문제

--- JS/step1/js/1_3_typeof.js
+++ JS/step1/js/1_3_typeof.js
@@ -28,3 +28,30 @@
 
     ! 이외 함수(function), 객체(object) 등이 존재.
 */
+// *case. 1 자신이 가장 좋아하는 숫자를 favorNumber변수에 담아 자료형을 점검해보자.
+function testTypeof(){
+    var favorNumber = 1;
+    console.log(typeof(favorNumber));
+}
+// *case.2 자신의 영어이름을 engName 변수에 담아 자료형을 점검해보자.
+function textTypeof(){
+    var engName = "Mark";
+    console.log(typeof(engName));
+}
+// *case.3 현재 자신이 춥다,아니다를 cold 변수에 담아 자료형을 점검해보자.
+function check1(){
+    var cold = false;
+    console.log('cold 변수에 현재 담긴 값인' + cold + '의 자료유형은' + typeof(cold) + '입니다.');
+}
+// todo.4 현재 로그인 상태가 아니다를 loginStatus 변수에 담아 자료형을 check 해보자.
+function check2(){
+
+}
+// todo.5 test 변수를 선언하고 초기화 하지 않은 상태에서 자료형을 check 해보자.
+function check3(){
+
+}
+// todo.6 obj 변수에 객체를 담을 예정이니 null로 초기화해서 자료형을 check 해보자.
+function check4(){
+    
+}

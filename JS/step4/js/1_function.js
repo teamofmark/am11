081f1a760710bcbbd87108b746ba88cb8acca0f3
Mark <teamofmark@gmail.com>
2023-07-24 11:58:41 +0900

    arguments 진입전

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -8,9 +8,9 @@
 
 //* exp.1 다음 구문을 hello() 라는 함수를 만들어 유지보수 하기 쉽게 만들어보기
 
-//document.write("안녕. 방가와","<br>");
-//document.write("안녕. 방가와","<br>");
-//document.write("안녕. 방가와","<br>");
+// document.write("안녕. 방가와","<br>");
+// document.write("안녕. 방가와","<br>");
+// document.write("안녕. 방가와","<br>");
 
 function helloExp(){ //! 반복문 사용시
     var comment = "안녕. 방가와!"
@@ -24,3 +24,19 @@ function helloFunc(){//!함수로 호출시
     document.write(comment,"<br>");
 }
 //helloFunc();helloFunc();helloFunc();
+
+// *case.1 매개변수(Parameter)가 있는 함수 만들기.
+// ?함수내 정해진 변수의 value(값)를 변경하여, 기존의 함수가 실행된 이후 접근이 불가한 부분을 해결하기 위함.
+function sumParam(num1,num2){
+    var result = num1 + num2;
+    // var result = 101 + 102;
+    document.write('두 수의 합은 = ' + result);
+}
+// *case.2 매개변수의 자리가 없어도 있는 것처럼. - arguments(가변인자함수 - 배열인듯 배열아닌 배열같은)
+/*
+    ? arguments
+    * arguments 객체(object)는 모든 함수 안에서 사용하는 지역변수.
+    * arguments를 이용해서 함수내 인수들을 참조 할 수 있고 이 객체의 함수에 전달된 각 인수항목을 포함한다.
+    * 단, arguments는 array와 비슷해보이지만 length를 구하는 방식을 제외하고는 array와는 다르다.
+    * 필요에 따라 array로 추가 변환하여 사용 할 수 있다.
+*/

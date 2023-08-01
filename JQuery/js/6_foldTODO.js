af92c5371a279bcdb77654d1ced71ebb5eb912aa
Mark <teamofmark@gmail.com>
2023-08-01 12:28:27 +0900

    이미지 n열로 배치하기

---
+++ JQuery/js/6_foldTODO.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // todo. 1 한개의 함수만으로 완성시키기. -> .replace(); / gallery();
+    // todo. 2 한개의 함수를 버튼의 갯수만큼 호출해서 완성시키기. ex> 함수호출이 10번. $(x).click(function);
+    
+});
+function gallery(){
+    
+}

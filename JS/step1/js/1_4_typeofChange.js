de2ce37a1189a34353f448d14dc268fea21731f9
Mark <teamofmark@gmail.com>
2023-11-10 12:25:07 +0900

    형변환문제풀이완료

--- JS/step1/js/1_4_typeofChange.js
+++ JS/step1/js/1_4_typeofChange.js
@@ -88,17 +88,20 @@ function numbString(){
 // todo. 1-1 암시적형변환
 function todo1(){
     var test = 100;
-
+    var result = test + "";
+    console.log(typeof(result) + "=" + result);
 }
 // todo. 1-2 String 형변환
 function todo2(){
     var test = 100;
-    
+    var result = String(test);
+    console.log(typeof(result) + "=" + result);
 }
 // todo. 1-3 toString 형변환
 function todo3(){
     var test = 100;
-    
+    var result = test.toString();
+    console.log(typeof(result) + "=" + result);
 }
 
 // todo.2 문자 형을 숫자 형으로 변환하여 연산이 가능하게 만들기.
@@ -106,10 +109,12 @@ function todo3(){
 // todo. 2-1 parse 형변환
 function todo4(){
     var test="100.51";
-    var result = test + 150 + "20" //todo 숫자 연산이 되게끔 - 정수형(.51불필요)
+    var result = parseInt(test) + 150 + parseInt("20"); //todo 숫자 연산이 되게끔 - 정수형(.51불필요)
+    console.log(typeof(result) + "=" + result);
 }
 // todo. 2-2 Number 형변환
 function todo5(){
     var test = "100.51";
-    var result = test + 150 + "20" //todo 숫자 연산이 되게끔 - 실수형
+    var result = Number(test) + 150 + Number("20"); //todo 숫자 연산이 되게끔 - 실수형
+    console.log(typeof(result) + "=" + result);
 }

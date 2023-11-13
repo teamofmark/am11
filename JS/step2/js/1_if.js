723475123babe0a017c794dbc603422979c2faee
Mark <teamofmark@gmail.com>
2023-11-13 12:58:37 +0900

    홀수 / 짝수 판별완성하기

--- JS/step2/js/1_if.js
+++ JS/step2/js/1_if.js
@@ -15,3 +15,65 @@
 	alert('종료');
     
 */
+// *case. 1 현재시각이 오후인가 오전인가
+function amORpm(){
+    var date = new Date();
+    var hour = date.getHours();
+    // console.log(hour);
+
+    if(hour < 12){
+        alert('오전이야.');
+    }
+    // ! 위,아래의 조건은 연결되어있지 않음.
+    if(hour >= 12){
+        alert('오후야');
+    }
+}
+// *case.2 말일이냐 아니냐
+function lastDay(){
+    var date = new Date();
+    var day = date.getDate();
+    // console.log(day);
+    if(day<30){
+        alert('아직 말일이 아닙니다.');
+    }else{
+        alert('말일 입니다.');
+    }
+}
+// todo. 아침먹을? 점심먹을? 저녁먹을? 시간인지 판별
+function mealTime(){
+    var date = new Date();// todo. 시간 가져오기
+    var hour = date.getHours();
+
+    if(hour < 11){
+        alert("아침먹어");
+    }
+    // todo. 아침먹을시간 alert으로 띄우기
+    else if(hour < 15){
+        alert("점심먹어");
+    }
+    // todo. 점심먹을시간 alert으로 띄우기
+    else{
+        alert("그만먹어");
+    }
+    // todo. 저녁먹을시간 alert으로 띄우기
+}
+// *case.3 양,음수 판별
+function numbJudgment(){
+    var numb = prompt('숫자를입력하세요.',"양,음수상관없음. 숫자만!!!");
+    console.log(typeof(numb) + "=" + numb);
+    numb = parseFloat(numb);
+    console.log(typeof(numb) + "=" + numb);
+    if(numb > 0){
+        alert("양수.");
+    }else if(numb < 0){
+        alert("음수.");
+    }else{
+        alert("0이거나 잘못된 값입니다.");
+    }
+
+}
+// todo. 위 방식같이 숫자를 입력받고 홀수인지 짝수인지 판별하는 함수 완성하기
+function oddOrEven(){
+    
+}

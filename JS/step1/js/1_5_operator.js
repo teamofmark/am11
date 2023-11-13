24181e680e4fe83fac1fc87aaf3683f53fe82bc9
Mark <teamofmark@gmail.com>
2023-11-13 11:45:45 +0900

    비교연산자 실습

--- JS/step1/js/1_5_operator.js
+++ JS/step1/js/1_5_operator.js
@@ -70,7 +70,64 @@ function operatorEx(){
 // *case.4 연산자가 뒤로 올 경우
 function numberPlusAfter(){
     var number = 10;
-    alert(number++); //? 예상결과 : . 확인결과 : .
-    alert(number++); //? 예상결과 : . 확인결과 : .
-    alert(number++); //? 예상결과 : . 확인결과 : .
+    console.log(number);
+    alert(number++); //? 예상결과 : 11. 확인결과 : 10. - 10을 출력후 더해서 다시 number-
+    console.log(number);
+    alert(number++); //? 예상결과 : 12. 확인결과 : 11. - 11을 출력후 더해서 다시 number-
+    console.log(number);
+    alert(number++); //? 예상결과 : 13. 확인결과 : 12. - 12를 출력후 더해서 다시 number-
+    console.log(number);
+}
+// *case.5 예상한 결과대로
+function numberPlusBefore(){
+    var number = 10;
+    alert(++number);
+    alert(++number);
+    alert(++number);
+}
+// todo. 증감연산자복합.
+function numberPlusTodo(){
+    var number = 10;
+    console.log(number++); //? 예상: 10 / 확인:
+    console.log(--number); //? 예상: 10 / 확인:
+    console.log(++number); //? 예상: 11 / 확인:
+    console.log(number--); //? 예상: 11 / 확인:
+    console.log(number++); //? 예상: 10 / 확인:
+    console.log(++number); //? 예상: 12 / 확인:
+    console.log(--number); //? 예상: 11 / 확인:
+    console.log(number++); //? 예상: 11 / 확인:
+    console.log(++number); //? 예상: 13 / 확인:
+    console.log(number); //? 예상: 13 / 확인:
+}
+// *case.6 숫자 10이 담긴 변수 a와 숫자 20이 담긴 변수b의 크기를 비교하여 출력.
+function compareOperator1(){
+    var a = 10;
+    var b = 20;
+    var result = a>b;
+    console.log("10 > 20 = " + result);
+}
+// *case.7 숫자 10이 담긴 변수 a와 숫자 20이 담긴 변수b의 크기가 같은지 비교하여 출력.
+function compareOperator2(){
+    var a = 10;
+    var b = 20;
+    var result = a!=b;
+    console.log("10 != 20 ? " + result);
+}
+// *case.8 a변수에는 '여자', b변수에는 '웹디자이너'라는 값이 저장되어 있다. 담겨있는 두 값이 모두 맞는지 판별.
+function logicalOperator1(){
+    var a = "여자";
+    var b = "웹퍼블리셔";
+    var result = (a=="여자")&&(b=="웹디자이너");
+    console.log("result = " + result);
+}
+// *case.9 a변수에는 '남자', b변수에는 '웹퍼블리셔'라는 값이 저장되어 있다. 담겨있는 두 값중 하나라도 맞는지 판별.
+function logicalOperator2(){
+    var a = '남자';
+    var b = '웹퍼블리셔';
+    var result = (a=="남자")||(b=="웹퍼블리셔");
+    console.log("result = " + result);
+}
+// todo. 다음 코드의 결과를 예상한 다음 출력하라. 그리고 문제를 해결하기.
+function compareOperator3(){
+    console.log(30>20>10); //? 예상결과 : true
 }

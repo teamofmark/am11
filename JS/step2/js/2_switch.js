e674ad2f29260c1c44f6dcdd51547224e15f1a72
Mark <teamofmark@gmail.com>
2023-11-15 11:39:12 +0900

    switch 문을 이용한 간단한 계산기

--- JS/step2/js/2_switch.js
+++ JS/step2/js/2_switch.js
@@ -22,3 +22,48 @@ function switchGrammar(){
     }
 }
 // todo. lotto과제 (if문) switch문으로 한번 풀어보기
+function lotto(){
+    var numbValue = window.prompt("입력하실번호는?");
+    switch(numbValue){
+        case "1":
+            alert("1등 10억원!");
+            break;
+        case "2":
+            alert("2등 5천만원!");
+            break;
+        case "3":
+            alert("3등 3백만원!");
+            break;
+        default:
+            alert("낙첨되었습니다.");
+            break;
+    }
+}
+// todo. switch문을 이용한 계산기
+function calculator(){
+    var firstNumb = prompt("첫번째 숫자를 입력하세요.");
+    var operator = prompt("사칙연산자중 하나를 입력하세요.");
+    var lastNumb = prompt("두번째 입력하세요.");
+
+    // ? 형변환
+    firstNumb = Number(firstNumb);
+    lastNumb = Number(lastNumb);
+
+    switch(operator){
+        case "+":
+            alert(firstNumb + lastNumb);
+            break;
+        case "-":
+            alert(firstNumb - lastNumb);
+            break;
+        case "*":
+            alert(firstNumb * lastNumb);
+            break;
+        case "/":
+            alert(firstNumb / lastNumb);
+            break;
+        default:
+            alert("잘못된 값이 입력되었습니다.");
+            break;
+    }
+}

1c2c1a0e9278d4e89012ea0e4080d499969ca781
Mark <teamofmark@gmail.com>
2023-11-16 12:07:59 +0900

    실습문제풀이완료

--- JS/step3/js/1_for.js
+++ JS/step3/js/1_for.js
@@ -77,5 +77,38 @@ function reverseFavorFruit(){
 // todo. 기존 숫자로 출력되는 순서를 한글화 하여 출력하기.
 function arrayFor(){
     var data = ["변수", "연산자", "형변환", "조건문 if", "조건문 switch", "반복문 while", "반복문 for"];
-    
+    var swapNumb = '';
+    for(var i = 0; i < data.length; i++){
+        switch(i){
+            case 0:
+                swapNumb = '첫';
+                break;
+            case 1:
+                swapNumb = '두';
+                break;
+            case 2:
+                swapNumb = '세';
+                break;
+            case 3:
+                swapNumb = '네';
+                break;
+            case 4:
+                swapNumb = '다섯';
+                break;
+            case 5:
+                swapNumb = '여섯';
+                break;
+            case 6:
+                swapNumb = '일곱';
+                break;
+        }
+        document.write(swapNumb + "번째 내용 = " + data[i] + '<br>');
+    }
+}
+function arrayFor2(){
+    var data = ["변수", "연산자", "형변환", "조건문 if", "조건문 switch", "반복문 while", "반복문 for"];
+    var swapNumb = ["첫","두","세","네","다섯","여섯","일곱"];
+    for(var i = 0; i < data.length; i++){
+        document.write(swapNumb[i] + "번째 내용 = " + data[i] + "<br>");
+    }
 }

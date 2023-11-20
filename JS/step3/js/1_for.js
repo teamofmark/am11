3130b5d898bc4a2807cbb887aa2da9ca50d8c096
Mark <teamofmark@gmail.com>
2023-11-20 11:46:15 +0900

    총합구하기

--- JS/step3/js/1_for.js
+++ JS/step3/js/1_for.js
@@ -272,8 +272,17 @@ function pyramidEx(){
 }
 function gugudan(){
     // todo 입력받은 단수를 x1~x9까지 document.write로 출력하는 함수
+    var guguNumb = prompt('알고싶은 구구단수를 입력');
+    for(var i = 1; i <= 9; i++){
+        document.write(guguNumb + ' x ' + i + ' = ' + guguNumb*i + '<br>');
+    }
 }
 function arraySum(){
     // todo 담긴 배열의 총합을 document.write로 출력하는 함수
     var data = [10,20,30,40,50];
+    var result = 0;
+    for(let i = 0; i < data.length; i++){
+        result += data[i];
+    }
+    document.write('해당 배열의 총합은 = ' + result);
 }

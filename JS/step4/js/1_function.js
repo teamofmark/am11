bf0c558cf4a923e79ff6717283c0072e96d30189
Mark <teamofmark@gmail.com>
2023-11-20 12:56:17 +0900

    return 기본 개념

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -19,3 +19,57 @@ function sumParam(num1,num2){
     var result = num1 + num2;
     alert('두 수의 합은 = ' + result);
 }
+// *case. 3 매개변수의 자리가 없어도 있는 것처럼... arguments(가변인자함수)
+/*
+    ? arguments
+    * arguments 객체(object)는 모든 함수 안에서 사용하는 지역변수.
+    * arguments를 이용해서 함수내 인수들을 참조 할 수 있고 이 객체의 함수에 전달된 각 인수항목을 포함한다.
+    * 단, arguments는 array와 비슷해보이지만 length를 구하는 방식을 제외하고는 array와는 다르다.
+    * 필요에 따라 array로 추가 변환하여 사용 할 수 있다.
+*/
+function sumAll(){
+    // console.log(typeof(arguments) + ':' + arguments.length);
+    var willReturn = 0;
+    for(var i = 0; i<arguments.length; i++){
+        willReturn += arguments[i];
+    }
+    console.log(willReturn);
+}
+function sumAllEX(){
+    var willReturn = 0;
+    for (var i in arguments){
+        willReturn += arguments[i];
+    }
+    return willReturn;
+}
+// alert(sumAllEX(1,2,3,4,5,6,7,8,9,10));
+document.write(sumAllEX(1,2,3,4,5,6,7,8,9,10));
+/*
+    ? return (리턴)
+    * 함수 내부는 함수라는 범위 안에 갇히기 때문에, 한번 실행되면 함수 외부에서 접근 불가.
+    * 이 때 매개변수를 활용하거나 arguments를 활용하면 함수 내부로 data를 전달 할 수 있는데,
+    * returnValue는 이와 반대 되는 개념.
+    * 즉, 매개변수 값이 입력부라면 return 값은 함수 내부에서 외부로 나가는 출력부.
+*/
+function f(x){
+    // console.log(x * x);
+    return x * x;
+}
+// *case. 4 무한루프를 돌며 숫자를 입력받고 입력받은 수의 합을 화면에 출력하는 함수.
+// ! 단, 입력값이 0이면 즉시 실행을 멈추게 한다.
+function infiniteSum(){
+    var sum = 0;
+    var count = 1;
+
+    while(true){
+        var value = parseInt(window.prompt("숫자만 입력해라."));
+        if(value == 0){
+            document.write("종료");
+            return;
+        }
+        sum += value;
+        document.write(count + ". " + sum + "<br>");
+        count++;
+    }
+    document.write("총" + count + "번  실행함.");
+}

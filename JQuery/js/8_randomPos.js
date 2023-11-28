5509d084711260ca8442df3afe9a9619393e33bb
Mark <teamofmark@gmail.com>
2023-11-28 12:55:56 +0900

    random pos 진행전

---
+++ JQuery/js/8_randomPos.js
@@ -0,0 +1,16 @@
+var count = 0;
+var $score = null;
+var $circle = null;
+var $btnStart = null;
+var $panel = null;
+var playState = false;
+var timerID = 0;
+$(document).ready(function(){
+    init();
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $btnStart = $("#btn_start");
+    $panel =  $(".panel");
+}

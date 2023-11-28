2bc5d099c4b5db7def0e7765e980dde6ea240df3
Mark <teamofmark@gmail.com>
2023-11-28 11:22:46 +0900

    실습문제 패턴 설명

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     imgAlignHori();
+    imgAlignMix();
 });
 function imgAlignHori(){
     var img = $(".imgContainer img");
@@ -12,5 +13,30 @@ function imgAlignHori(){
             xpos = imgWidth * i;
             image.css("left",xpos);
         }
+    });
+    $(".reset").click(function(){
+        img.css("left",0);
+    });
+}
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+    $(".alignMix").click(function(){
+        // todo. for loop. - 이미지 개수보다 작을때 까지 0부터 시작해서 반복.
+            // todo. "현재이미지변수"에 n번째 이미지 담기.
+            // todo. xpos 상황
+            // ? 0(0), 1(1), 2(2),
+            // ? 3(0), 4(1), 5(2),
+            // ? 6(0), 7(1), 8(2),
+            // ? 9(0), 10(1), 11(2)
+            // ? 12(0)............
+            // todo. ypos 상황
+            // ? 0(0), 1(0), 2(0),
+            // ? 3(1), 4(1), 5(1),
+            // ? 6(2), 7(2), 8(2),
+            // ? 9(3), 10(3), 11(3)
+            // ? 12(4)............
+            // todo. "현재이미지변수"에 xpos = left, ypos = top으로 css 적용.
     });
 }

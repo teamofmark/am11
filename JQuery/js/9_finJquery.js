f80e97b2e9c68777cc7c642203b956b9a350c12f
Mark <teamofmark@gmail.com>
2023-11-29 12:56:49 +0900

    세부기능 진행전

--- JQuery/js/9_finJquery.js
+++ JQuery/js/9_finJquery.js
@@ -6,6 +6,7 @@ var $fishWidth = 0;
 $(function(){
     init();
     fishStartPosition(); //? 물고기 시작 위치 설정
+    initEvent();
 });
 function init(){
     $fishList = $(".fish");
@@ -22,4 +23,26 @@ function fishStartPosition(){
             top: 100 + (i * 150)
         });
     }
+    console.log("2. 초기위치설정완료.");
+}
+function initEvent(){
+    $("#start").click(startGame);
+}
+function startGame(){
+    if(timerID == -1){
+        timerID = setInterval(function(){
+                    updateFishPosition(); //? 물고기 움직이는 함수
+                    displayFishPositionInfo(); //? 움직인 물고기 위치 기록 하는 함수 
+                    checkGoalFish(); //? 결승선 도달 감지 하는 함수
+                },200);
+    }
+}
+function updateFishPosition(){
+
+}
+function displayFishPositionInfo(){
+
+}
+function checkGoalFish(){
+
 }

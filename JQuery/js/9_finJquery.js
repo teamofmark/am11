3b36cb0572a2710acdf6f82b5a61fe08448b6f9c
Mark <teamofmark@gmail.com>
2023-11-30 11:38:33 +0900

    jquery 최종예제 완성

--- JQuery/js/9_finJquery.js
+++ JQuery/js/9_finJquery.js
@@ -39,11 +39,46 @@ function startGame(){
     }
 }
 function updateFishPosition(){
+    for(var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        var step = Math.ceil(Math.random()*30);
+        var newLeft = $fish.position().left + step;
 
+        $fish.css("left", newLeft);
+    }
+    console.log("4. 물고기 움직임 정상.");
 }
 function displayFishPositionInfo(){
-
+    var info = "";
+    for(var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        info += i + "번 물고기 : " + $fish.position().left + "px <br>";
+    }
+    $info.html(info);
+    console.log("5. 물고기 위치 표시 정상.");
 }
 function checkGoalFish(){
-
+    var winnerList = [];
+    for(var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+                index: (i), position: fishCurrentPosition
+            });
+            console.log("6. 데이터 저장 성공.");
+        }
+    }
+    if(winnerList.length > 0){
+        winnerList.sort(function(a,b){
+            return b.position - a.position; //? position 기준 내림차순 정렬.
+        });
+        endGame();
+        console.log("7. 우승 물고기 선별 완료.");
+        alert("우승!" + winnerList[0].index +"번 물고기");
+    }
+}
+function endGame(){
+    clearInterval(timerID);
+    timerID = -1;
+    console.log("8. 게임 종료.");
 }

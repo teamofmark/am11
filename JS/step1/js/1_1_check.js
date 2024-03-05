71f6b4966caee6b33ed04514c2b3e7bd59a425d9
Mark <teamofmark@gmail.com>
2024-03-05 12:23:47 +0900

    오류검출하는법

---
+++ JS/step1/js/1_1_check.js
@@ -0,0 +1,11 @@
+function runAlert(){
+    alert("경고창으로 검수하기");
+}
+
+// todo 밑에 두 실행문을 함수로 감싸 실행시에만 나오게 만들기
+function runLog(){
+    console.log("콘솔창으로 검수하기");
+}
+function runWrite(){
+    document.write("html문서로 검수하기");
+}

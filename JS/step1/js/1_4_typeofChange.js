8c6f0d002e9e80767f378433cbc0ab73e5c1c4c6
Mark <teamofmark@gmail.com>
2024-03-12 11:32:21 +0900

    jquery CDN 배포

--- JS/step1/js/1_4_typeofChange.js
+++ JS/step1/js/1_4_typeofChange.js
@@ -18,3 +18,44 @@ function agePlus(){
     var result = Number(age) + 20; //? 전역함수 Number
     console.log(result);
 }
+// *case.2 강제 숫자 -> 문자형변환
+function forcedString(){
+    var a = '30';
+    var result = 1 + a + 10; //? 1, 문자 30, 10을 더해라.
+    console.log("result의 자료유형은" + typeof(result) + " 값은" + result);
+}
+// *case.3 강제 불린 -> 숫자형변환
+function forcedNumber(){
+    var result = 2 + true; //? true = 1, false = 0.
+    console.log("result의 자료유형은" + typeof(result) + " 값은" + result);
+}
+// *case.4 강제 불린 -> 문자형변환
+function forcedString2(){
+    var a ='2';
+    var result = a + true;
+    console.log("result의 자료유형은" + typeof(result) + " 값은" + result);
+}
+// *case.5 test1에 들어있는 숫자를 문자로 명시적 형변환.
+/*
+    ? String();
+    * String(value); - 전역함수 -> 모든 자료형을 문자로 변환.
+
+    ? .toString();
+    * Number.toString(); - 문자형을 진수변환이 필요할때
+
+    ? .toFixed();
+    * Number.toFixed(); - (소수점자리를 반올림하는) 실수문자형이 필요할 때.
+*/
+function stringNumber(){
+    var test1 = 15;
+    console.log("변환 전 : " + typeof(test1) + ' = ' + test1);
+    var result = String(test1);
+    console.log("변환 후 : " + typeof(result) + ' = ' + result);
+}
+function stringtoNumb(){
+    var test1 = 15;
+    console.log("변환 전 : " + typeof(test1) + ' = ' + test1);
+    var result = test1.toString();
+    console.log("변환 후 : " + typeof(result) + ' = ' + result);
+}
+// ! tune. background-color random

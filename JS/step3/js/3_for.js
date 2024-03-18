2e844a2a7b2d292fa7c7bc9c77b5a2c891b52af9
Mark <teamofmark@gmail.com>
2024-03-18 12:16:20 +0900

    답안 5종

--- JS/step3/js/3_for.js
+++ JS/step3/js/3_for.js
@@ -39,14 +39,23 @@ function answer1(){
     }
 }
 function answer2(){
-
+    for(var i = 0; i<10; i++){
+        document.write((i+1)+"<br>");
+    }
 }
 function answer3(){
-
+    for(var i = 100; i<110; i++){
+        document.write((i-99)+"<br>");
+    }
 }
 function answer4(){
-
+    for(var i = 10; i>=1; i--){
+        document.write((11-i)+"<br>");
+    }
 }
 function answer5(){
-
+    for(var i = 1; i<=10; i+=2){
+        document.write(i+"<br>");
+        document.write((i+1)+"<br>");
+    }
 }

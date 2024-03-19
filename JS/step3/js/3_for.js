37abf565a6a27239afdce7fd909b0b50f11055c0
Mark <teamofmark@gmail.com>
2024-03-19 11:18:53 +0900

    과제풀이 공유

--- JS/step3/js/3_for.js
+++ JS/step3/js/3_for.js
@@ -70,6 +70,10 @@ function favorFruit(){
 // todo. 지금까지 배웠던 JS 단원명을 배열을 활용해 출력하되, 숫자가 아닌 한글순서로 출력되게.
 function arrayFor(){
     var data = ["변수", "연산자", "형변환", "조건문 if", "조건문 switch", "반복문 while", "반복문 for"];
+    var swapNumb = ["첫","두","세","네","다섯","여섯","일곱"];
+    for (var i = 0; i < data.length; i++) {
+        document.write(swapNumb[i] + "번째 내용 =" + data[i] + '<br>');
+    }
     /*
         * 첫번째 내용 =변수
         * 두번째 내용 =연산자
@@ -79,4 +83,38 @@ function arrayFor(){
         * 여섯번째 내용 =반복문 while
         * 일곱번째 내용 =반복문 for
     */
+}
+function arrayFor2(){
+    var data = ["변수", "연산자", "형변환", "조건문 if", "조건문 switch", "반복문 while", "반복문 for"];
+    var swapNumb = '';
+    for(var i = 0; i < data.length; i++){
+        swapNumb = i;
+        switch(i){
+            case 0:
+                swapNumb = '첫';
+                break;
+            case 1:
+                swapNumb = '두';
+                break;
+            case 2:
+                swapNumb = '세';
+                break;
+            case 3:
+                swapNumb = '네';
+                break;
+            case 4:
+                swapNumb = '다섯';
+                break;
+            case 5:
+                swapNumb = '여섯';
+                break;
+            case 6:
+                swapNumb = '일곱';
+                break;
+            default:
+                i = 'x';
+                break;
+        }
+        document.write(swapNumb + "번째 내용 =" + data[i] + '<br>');
+    }
 }

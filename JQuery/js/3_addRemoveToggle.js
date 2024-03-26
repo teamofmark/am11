340ddcba0915f76bd41fa73b21aef6e5471e5b09
Mark <teamofmark@gmail.com>
2024-03-26 12:19:49 +0900

    tab 구조화 완성

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     toggleCheck();
+    toggleTodo();
 });
 function toggleCheck(){
     var $checkTarget = $(".material-icons.checkIco");
@@ -15,6 +16,27 @@ function toggleCheck(){
             */
         }else{
             $checkTarget.text("check_box_outline_blank");
+        }
+    });
+}
+// todo. 
+/*
+    * 1. 눈 켜진 상태 text를 찾아야지 -> material에서..
+    * 2. type='password' -> 'text' 변경방법? -> target.attr("type","text") = type을 text로 변경해라.
+*/
+function toggleTodo(){
+    var $toggleTarget = $(".material-icons.toggleVisible");
+    var $toggleInput = $("#userPW");
+    var toggleStatus = false;
+
+    $toggleTarget.click(function(){
+        toggleStatus = !toggleStatus;
+        if(toggleStatus == true){
+            $toggleTarget.text("visibility");
+            $toggleInput.attr("type","text");
+        }else{
+            $toggleTarget.text("visibility_off");
+            $toggleInput.attr("type","password");
         }
     });
 }

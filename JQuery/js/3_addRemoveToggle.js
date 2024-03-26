89565d3cc8429fc54023e6d39d3ab4cc4c8e7467
Mark <teamofmark@gmail.com>
2024-03-26 11:46:12 +0900

    실습재료 배포

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
 });
 function toggleCheck(){
     var $checkTarget = $(".material-icons.checkIco");
+    var $checkLabel = $(".label.checkLabel");
+    var checkStatus = false;
+    $checkLabel.click(function(){
+        checkStatus = !checkStatus;
+        // console.log(checkStatus);
+        if(checkStatus == true){
+            $checkTarget.text("check_box");
+            /*
+                ? target.text(); : text 불러와. / target.text("blah"); : text를 blah로 교체해.
+            */
+        }else{
+            $checkTarget.text("check_box_outline_blank");
+        }
+    });
 }

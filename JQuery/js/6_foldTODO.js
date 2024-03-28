1e41f7c4d7d9e478320530e2fb79c4813dcbc48d
Mark <teamofmark@gmail.com>
2024-03-28 12:30:27 +0900

    fold 과제 마지막

--- JQuery/js/6_foldTODO.js
+++ JQuery/js/6_foldTODO.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
     gallery2(0,200,200);
-    // ? 초기 set - 
+    // ? 초기 set - 열수,너비,높이
+    // todo. 1열 버튼 click시 해당 열로 배치.
+    // todo. 2열 버튼 click시 해당 열로 배치.
+    // todo. 3열 버튼 click시 해당 열로 배치.
+    // todo. 4열 버튼 click시 해당 열로 배치.
+    // todo. 5열 버튼 click시 해당 열로 배치.
+    // todo. 6열 버튼 click시 해당 열로 배치.
+    // todo. 7열 버튼 click시 해당 열로 배치.
+    // todo. 8열 버튼 click시 해당 열로 배치.
+    // todo. 9열 버튼 click시 해당 열로 배치.
+    // todo. 10열 버튼 click시 해당 열로 배치.
+
+    // ! replace - JQuery 함수 검색해서 한번의 호출로 n개의 버튼 소화하기.
 });
-function gallery2(){
+function gallery2(count,imgWidth,imgHeight){
 
 }

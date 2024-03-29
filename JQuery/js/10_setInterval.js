c5a5940526769746e51cf6c9dd5fe2cb344ab852
Mark <teamofmark@gmail.com>
2024-03-29 12:46:57 +0900

    setInterval 종료

---
+++ JQuery/js/10_setInterval.js
@@ -0,0 +1,39 @@
+// ? setInterval(function,interval);
+var $circle = null;
+var railWidth = 0;
+var runStep = 10;
+var timerID = 0; //? setInterval을 담을 변수.
+
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $circle = $("#circle");
+    railWidth = $("#rail").width();
+}
+function initEvent(){
+    $("#btn_start").click(moveStart);
+    $("#btn_stop").click(moveStop);
+}
+function moveStart(){
+    if(timerID == 0){
+        timerID = setInterval(moveCircle,1);
+    }
+}
+function moveCircle(){
+    var xpos = $circle.position().left; //? 현재 position left 구하기. - left,top.
+    xpos += runStep;
+    $circle.css({
+        left: xpos
+    })
+    if(xpos >= railWidth || xpos <= 0){
+        runStep *= -1;
+    }
+    // todo. 집나간 원잡아오기 - 오른쪽으로가다가? rail끝에 위치하면 반대로- 반대편 끝에 위치하면? 또 반대로-
+
+}
+function moveStop(){
+    clearInterval(timerID); // ? float: left; -> clear: left;
+    timerID = 0;
+}

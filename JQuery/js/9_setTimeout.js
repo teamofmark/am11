3987fa636815a743023ebb2ee0669fd7b21c93f2
Mark <teamofmark@gmail.com>
2024-03-29 11:26:17 +0900

    setTimeout todo 배포

---
+++ JQuery/js/9_setTimeout.js
@@ -0,0 +1,33 @@
+// ? setTimeout(function,timer); - 일정시간 경과후 함수실행.
+var count = 0;
+var $score = null;
+var $circle = null;
+var playState = true;
+var $resetBtn = null;
+
+$(document).ready(function(){
+    init(); //? 선언부
+    scoreCount($circle,playState,count,$score);
+    //todo. button에 물려야함. restart;
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $resetBtn = $(".resetBtn");
+}
+function scoreCount($circle,playState,count,$score){
+    $circle.click(function(){
+        if(playState == true){
+            count++;
+            $score.text(count+"점");
+        }
+    });
+    setTimeout(function(){
+        alert("게임종료.");
+        playState = false;
+    },5000);
+}
+// todo. 재시작기능 만들어보기 - restart
+function restart(){
+
+}

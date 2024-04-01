ae5964c70efab148400d72febf6d6fdd31a271ce
Mark <teamofmark@gmail.com>
2024-04-01 11:09:43 +0900

    interval

--- JQuery/js/10_setInterval.js
+++ JQuery/js/10_setInterval.js
@@ -37,3 +37,35 @@ function moveStop(){
     clearInterval(timerID); // ? float: left; -> clear: left;
     timerID = 0;
 }
+/*
+$(document).ready(function(){
+    var $circle = $("#circle");
+    var runStep = 10;
+    var xpos = $circle.position().left;
+    var railWidth = $("#rail").width();
+    var timerID = 0;
+    
+    startMove($circle, runStep, xpos, railWidth, timerID);
+});
+
+function startMove($circle, runStep, xpos, railWidth, timerID){
+    $("#btn_start").click(function(){
+        timerID = setInterval(function(){
+                    xpos += runStep;
+    
+                    $circle.css("left",xpos);
+    
+                    if(xpos > railWidth || xpos < 0){
+                        runStep *= -1;
+                    }
+                },0);
+        stopMove(timerID);
+    });
+}
+
+function stopMove(timerID){
+    $("#btn_stop").click(function(){
+        clearInterval(timerID);
+    });
+}
+*/

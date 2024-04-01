f0e68d3a8695e42a4b7436123fb82e66cff020cd
Mark <teamofmark@gmail.com>
2024-04-01 11:34:48 +0900

    요구조건

---
+++ JQuery/js/12_attrControl_todo.js
@@ -0,0 +1,13 @@
+var logos = ["logo_01.jpg","logo_02.jpg","logo_03.jpg","logo_04.jpg","logo_05.jpg","logo_06.jpg","logo_07.jpg","logo_08.jpg"];
+
+
+// ? 선언부
+
+$(document).ready(function(){
+    // todo. swap 함수를 2번 실행하여 각각 1초,3초로 작동할 수 있게.
+});
+
+// todo. 함수 생성은 한번만.
+function swap(){
+    
+}

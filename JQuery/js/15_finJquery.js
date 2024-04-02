23719e0b0e27b4bff2daef3028dcf853771a6b1c
Mark <teamofmark@gmail.com>
2024-04-02 11:42:34 +0900

    결승선도달감지 함수 작성중

--- JQuery/js/15_finJquery.js
+++ JQuery/js/15_finJquery.js
@@ -32,9 +32,9 @@ function initEvent(){
 function startGame(){
     if(timerID == -1){
         timerID = setInterval(function(){
-            updateFishPosition();// ? 물고기 움직임
-            // ? displayFishPositionInfo();움직인 위치 표시
-            // ?checkGoalFish(); 결승선 도달 감지
+            updateFishPosition(); // ? 물고기 움직임
+            displayFishPositionInfo(); // ? 움직인 위치 표시
+            checkGoalFish(); // ?결승선 도달 감지
         },200);
         console.log("3. 게임 정상 시작.");
     }
@@ -47,4 +47,16 @@ function updateFishPosition(){
         $fish.css("left", newLeft);
     }
     console.log("4. 물고기 움직임 정상");
+}
+function displayFishPositionInfo(){
+    var info = "";
+    for(var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        info += i + "번 물고기 : " + $fish.position().left + "px <br>";
+    }
+    $info.html(info);
+    console.log("5. 물고기 위치 표시 정상");
+}
+function checkGoalFish(){
+    var winnerList = [];
 }

d7abe6e59b83f018bf69da93339894816a6870be
Mark <teamofmark@gmail.com>
2024-04-03 11:38:48 +0900

    speed&ticker 관계

--- plugin/js/customize.bxslider.js
+++ plugin/js/customize.bxslider.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $(".exSlider").bxSlider();
+    $(".exSlider").bxSlider({
+        mode: 'horizontal',  //? default : horizontal(수평방향전환), vertical(수직방향전환), fade(사라지고나타나고)
+        speed: 750, //? default : 500. 전환속도 제어
+        ticker: false, //? default : false. slider를 controler의 통제를 받지 않는 상태로 흘러가게 만들기. speed,mode의 영향을 받음.
+        tickerHover: false //? default : false. ticker 상태일 때 onMouseOver로 중지시키기.
+    });
 });

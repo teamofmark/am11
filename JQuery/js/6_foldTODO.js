c5aad787af4ece0e2e12f773e7e86f5afa76339a
Mark <teamofmark@gmail.com>
2024-04-04 13:10:57 +0900

    과제 풀이 배포

--- JQuery/js/6_foldTODO.js
+++ JQuery/js/6_foldTODO.js
@@ -1,19 +1,58 @@
 $(document).ready(function(){
-    gallery2(0,200,200);
-    // ? 초기 set - 열수,너비,높이
-    // todo. 1열 버튼 click시 해당 열로 배치.
-    // todo. 2열 버튼 click시 해당 열로 배치.
-    // todo. 3열 버튼 click시 해당 열로 배치.
-    // todo. 4열 버튼 click시 해당 열로 배치.
-    // todo. 5열 버튼 click시 해당 열로 배치.
-    // todo. 6열 버튼 click시 해당 열로 배치.
-    // todo. 7열 버튼 click시 해당 열로 배치.
-    // todo. 8열 버튼 click시 해당 열로 배치.
-    // todo. 9열 버튼 click시 해당 열로 배치.
-    // todo. 10열 버튼 click시 해당 열로 배치.
-
-    // ! replace - JQuery 함수 검색해서 한번의 호출로 n개의 버튼 소화하기.
+    gallery();
+    // gallery2(0,200,200);
+    // $(".1way").click(function(){gallery2(1,1000,1000)});
+    // $(".2way").click(function(){gallery2(2,500,500)});
+    // $(".3way").click(function(){gallery2(3,333,333)});
+    // $(".4way").click(function(){gallery2(4,250,250)});
+    // $(".5way").click(function(){gallery2(5,200,200)});
+    // $(".6way").click(function(){gallery2(6,166,166)});
+    // $(".7way").click(function(){gallery2(7,142,142)});
+    // $(".8way").click(function(){gallery2(8,125,125)});
+    // $(".9way").click(function(){gallery2(9,111,111)});
+    // $(".10way").click(function(){gallery2(10,100,100)});
 });
-function gallery2(count,imgWidth,imgHeight){
 
+function gallery2(count, imgWidth, imgHeight){
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+    var xpos, ypos;
+
+    for(i = 0; i<$imgLength; i++){
+        var $image = $images.eq(i); //? eq = DOM 요소 세트를 나타내는 jQuery 객체가 주어지면 .eq()메소드는 해당 세트 내의 한 요소에서 새 jQuery 객체를 구성한다.
+        
+        xpos = (i % count) * imgWidth;
+        ypos = parseInt(i / count) * imgHeight;
+
+        $image.css({
+            'left' : xpos,
+            'top' : ypos,
+            'width' : imgWidth,
+            'height' : imgHeight
+        })
+    }
+}
+function gallery(){
+    var currentWay,xpos, ypos;
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+    var containerW = $("#imgContainer").width();
+    var imgSize = $images.width();
+
+    $("[class$='way']").click(function(){
+        currentWay = parseInt($(this).attr("class").replace("way",""));
+        for(i = 0; i<$imgLength; i++){
+            var $image = $images.eq(i); //? eq = DOM 요소 세트를 나타내는 jQuery 객체가 주어지면 .eq()메소드는 해당 세트 내의 한 요소에서 새 jQuery 객체를 구성한다.
+            imgSize = containerW / currentWay;
+            xpos = (i % currentWay) * imgSize;
+            ypos = parseInt(i / currentWay) * imgSize;
+            
+            $image.css({
+                'left' : xpos,
+                'top' : ypos,
+                'width' : imgSize,
+                'height' : imgSize
+            })
+        }
+    });
 }

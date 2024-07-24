f6a411e35b7052f523fe4be7f7d89792952025cc
markStark <teamofmark@gmail.com>
2024-07-24 12:24:57 +0900

    자료유형점검하는방법

--- JS/step1/js/3_typeof.js
+++ JS/step1/js/3_typeof.js
@@ -28,3 +28,33 @@
 
     ! 이외 함수(function), 객체(object) 등이 존재.
 */
+// case. 1 자신이 가장 좋아하는 숫자를 favorNumber변수에 담아 자료형을 check해보자.
+function testTypeof(){
+    var favorNumber = 1;
+    console.log(typeof(favorNumber));
+}
+// case. 2 자신의 영어이름을 engName 변수에 담아 자료형을 check해보자.
+function textTypeof(){
+    var engName = "Mark";
+    console.log(typeof(engName));
+}
+// case. 3 현재 자신이 춥다(true), 아니다(false)를 cold 변수에 담아 자료형으로 출력 및 점검
+function check1(){
+    var cold = false;
+    console.log("추위를 느끼고 있나?" + cold + "(" + typeof(cold)+ ")");
+}
+// case. 4 현재 로그인 상태를 loginStatus 변수에 담아 자료형을 출력 및 점검
+function check2(){
+    var loginStatus = true;
+    console.log("로그인 된 상태 인가?" + loginStatus + "(" + typeof(loginStatus) + ")");
+}
+// case. 5 test 변수를 "선언"하고 초기화 하지 않은 상태에서 자료형을 check.
+function check3(){
+    var test;
+    console.log("초기화 하지 않은 변수의 자료유형은?" + typeof(test) + "입니다.");
+}
+// case. 6 obj 변수에 null로 초기화 하고 자료형을 check.
+function check4(){
+    var obj = null;
+    console.log("obj에 담긴 값의 자료 유형은" + typeof(obj) + "입니다.");
+}

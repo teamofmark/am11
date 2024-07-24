149fc0a6315aa1e4d6f5d94dc49ed736298cac18
markStark <teamofmark@gmail.com>
2024-07-24 12:50:47 +0900

    명시적 형변환 - 문자형으로

--- JS/step1/js/4_typeChange.js
+++ JS/step1/js/4_typeChange.js
@@ -11,3 +11,38 @@
     ? 명시적 형변환
     * method 나 lib Core func를 이용하여 강제로 숫자 -> 문자 로 변환.
 */
+// case. 1 자신의 나이를 입력받고 20을 더한 값 출력.
+function agePlus(){
+    var age = prompt('나이입력');
+    console.log("age 자료유형" + typeof(age));
+    var result = Number(age) + 20;
+    console.log(result);
+}
+// case. 2 강제 숫자 - 문자형변환
+function forcedString(){
+    var a = '30';
+    var result = 1 + a + 10;
+    console.log("result 의 자료유형은" + typeof(result) + " 값은 " + result);
+}
+// case. 3 강제 불리언 - 숫자형변환
+function forcedNumber(){
+    var result = 2 + true;
+    console.log("result 의 자료유형은" + typeof(result) + " 값은 " + result);
+}
+// case. 4 강제 불리언 - 문자형변환
+function forcedString2(){
+    var a = '2';
+    var result = a + true;
+    console.log("result 의 자료유형은" + typeof(result) + " 값은 " + result);
+}
+// case. 5 test1에 들어있는 숫자를 문자로 명시적 형변환.
+/*
+    ? String
+    * String(value); - 문자형으로 변환
+
+    ? .toString()
+    * Number.toString(); - 문자형(진수)변환
+
+    ? .toFixed()
+    * Number.toFixed(); - (소수점자리수(반올림발생))실수문자형 변환.
+*/

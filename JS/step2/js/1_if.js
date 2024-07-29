a9d1d630ae4fdd4cade4a1e7a15e42d5c26a40c2
markStark <teamofmark@gmail.com>
2024-07-29 12:54:13 +0900

    조건문 첫번째 과제

--- JS/step2/js/1_if.js
+++ JS/step2/js/1_if.js
@@ -1,5 +1,5 @@
 /*
-    조건문
+    조건문 (집합과명제)
     
     실무사용의 예
     1. 로그인 시 아이디와 패스워드가 서버에 저장된 정보와 같은지 비교 후 같다면 승인, 아니라면 오류메세지를 띄운다.
@@ -15,3 +15,43 @@
 	alert('종료');
     
 */
+// case.1 현재시각이 오후인가 오전인가?
+function amORpm(){
+    var date = new Date();
+    var hour = date.getHours();
+    // var hour = prompt("시간을 입력하세요");
+    console.log(hour);
+    if(hour < 12){// 조건문
+        alert('오전입니다.');// 오전입니다.
+    }else{
+        alert('오후입니다.');// 오후입니다.
+    }
+}
+// case.2 말일인가 아닌가
+function lastDay(){
+    var date = new Date();
+    var day = date.getDate();
+    console.log(day);
+    if(day<30){
+        alert('아직 말일 아님.');
+    }else{
+        alert('말일.');
+    }
+}
+function lastDayEx(){
+    function endOfMonth(date){
+        return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    }
+    dt = new Date(); 
+        
+    console.log(endOfMonth(dt).toString());
+}
+// todo. 아침?점심?저녁? 먹을시간 알려주기
+function mealTime(){
+    var date = new Date();
+    var hour; //todo 1. 현재 시간불러오기
+
+    // todo. 조건 - 내가 아침먹을 조건
+        // todo. 조건 - 내가 점심먹을 조건
+            // todo. 조건 - 내가 저녁먹을 조건
+}

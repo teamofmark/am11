1dd01d829414fde51b7cccc2709b027be50e477d
markStark <teamofmark@gmail.com>
2024-08-06 11:46:45 +0900

    return 기본

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -76,3 +76,23 @@ function showInfo(){
     * 이 때 매개변수를 활용하면 함수 내부로 data를 전달 할 수 있는데, return value는 이와 반대 되는 값.
     * 즉, 매개변수 값이 함수 외부에서 내부로 들어오는 입력 부라면, 리턴값은 함수 내부에서 처리한 결과를 함수 외부로 전달하기 위해 사용하는 출력 부.
 */
+// case. 6 함수f(x)안에 넣는 값이 매개변수. 그럼 출력은?
+function f(x){
+    return x*x;
+}
+/*
+console.log(f(3));
+alert(f(6));
+document.write(f(9));
+*/
+// case. 7 리턴되는 값을 변수에 저장하고 전달하기
+function sumReturn1(num1,num2){
+    var result = num1 + num2;
+    return result;
+}
+var value = sumReturn1(13,26);
+/*
+console.log(value);
+alert(value);
+document.write(value);
+*/

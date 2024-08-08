fad114a39d990f645678bdfb4512ce15eb06472e
markStark <teamofmark@gmail.com>
2024-08-08 12:08:55 +0900

    알고있는 부분까지

--- JS/step4/2_functionMiddle.js
+++ JS/step4/2_functionMiddle.js
@@ -13,3 +13,69 @@
     * 남는 경우가 있는데 이 현상을 클로저(closure)라 부르고, 이 현상을 일으키는 함수를 클로저함수.
     * 5. 멤버함수 - 멤버함수는 클래스 내부에 만들어 지며 이를 보통 메서드(method)라고 함.
 */
+// ! 중첩함수
+// ? 지역변수처럼 함수내부에서만 활용하는 함수. 중복 코드나 코드그룹핑을 내부에서 처리해야 할 경우
+// case. 1 1초에 한번씩 hi 출력.
+function sayHi(){
+    var count = 0;
+    setInterval(function(){
+        count++;
+        document.write(count + ". hi <br>");
+    },1000);
+}
+// case. 2 주의할 점
+function innerFunctionTest(){
+    var a = 10;
+    var b = 20;
+    var c = 30;
+    function outerFunc(){
+        var b = 200;
+        var c = 300;
+        function innerFunc(){
+            var c = 3000;
+            console.log("1. = " + a);
+            console.log("2. = " + b);
+            console.log("3. = " + c);
+        }
+        innerFunc();
+    }
+    outerFunc(); 
+}
+// case. 3 callBack 함수를 이용한 사칙연산기 만들기
+function calculatorCallBack(op,numb1,numb2,callBack){
+    var result = "";
+    switch(op){
+        case "+":{
+            result = add(numb1,numb2);
+            break;
+        }
+        case "-":{
+            result = sub(numb1,numb2);
+            break;
+        }
+        case "*":{
+            result = mul(numb1,numb2);
+            break;
+        }
+        case "/":{
+            result = div(numb1,numb2);
+            break;
+        }
+        default:{
+            result = "잘못된 입력값 입니다.";
+            break;
+        }
+    }
+};
+function add(numb1,numb2){
+    return numb1 + numb2;
+}
+function sub(numb1,numb2){
+    return numb1 - numb2;
+}
+function mul(numb1,numb2){
+    return numb1 * numb2;
+}
+function div(numb1,numb2){
+    return numb1 / numb2;
+}

4408428225b8f311ef632c5b8020b2660cd27915
markStark <teamofmark@gmail.com>
2024-08-14 11:51:46 +0900

    toggle 구현해보기

---
+++ JQuery/js/3_addRemoveToggle.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // case.1
+    toggleCheck();
+});
+function toggleCheck(){
+    var $checkTarget = $(".material-icons.checkIco");
+    var $checkLabel = $(".label.checkLabel");
+    // ! 실제 toggle 방식을 이해
+    var checkStatus = false;
+    $checkLabel.click(function(){
+        checkStatus = !checkStatus;
+        console.log(checkStatus);
+        if(checkStatus == true){
+            $checkTarget.text("check_box");
+        }else{
+            // todo
+        }
+    });
+}

bebc8b296ffdb84cd1a45dc1592596472dc09094
markStark <teamofmark@gmail.com>
2024-08-21 12:03:09 +0900

    timeout 완성

--- JQuery/js/9_setTimeout.js
+++ JQuery/js/9_setTimeout.js
@@ -6,3 +6,37 @@
         alert("펑!");
     },5000);
 */
+var count = 0;
+var $score = null;
+var $circle = null;
+var playState = true;
+var $resetBtn = null;
+$(document).ready(function(){
+    init();
+    scoreCount(count,$score,$circle,playState);
+    $resetBtn.click(restart);
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $resetBtn = $(".resetBtn");
+}
+function scoreCount(count,$score,$circle,playState){
+    alert("game Start!");
+    setTimeout(function(){
+        alert("game Over!");
+        playState = false;
+    },5000);
+    $circle.click(function(){
+        if(playState == true){
+            count++;
+            $score.text(count + "점");
+        }
+    });
+}
+function restart(){
+    playState = true;
+    count = 0;
+    $score.text(count + "점");
+    scoreCount(count,$score,$circle,playState);
+}

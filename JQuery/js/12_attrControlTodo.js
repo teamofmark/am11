f26825298a84be5cc84d2364adf478f8eb98644a
markStark <teamofmark@gmail.com>
2024-08-22 12:22:59 +0900

    문제 관련 구조 및 기본 스크립트

---
+++ JQuery/js/12_attrControlTodo.js
@@ -0,0 +1,17 @@
+/*
+    todo. 1 일정한 시간에 한 번씩 배열에 들어있는 이미지를 순차순환 시켜야 한다.
+    todo. 2 전환 시간을 각 이미지 별로 설정할 수 있게 해야함.
+    todo. 3 이미지가 계속해서 롤링 될 수 있도록 만들어야함.
+    todo. 4 하나의 함수를 활용해 n개의 배너가 동작 할 수 있도록 만들어야 함.
+*/
+var logos = ["logo_01.jpg","logo_02.jpg","logo_03.jpg","logo_04.jpg","logo_05.jpg","logo_06.jpg","logo_07jpg","logo_08.jpg"];
+var speed = 0;
+var $banner1 = null;
+var $banner2 = null;
+var currentIndex1 = 0;
+var currentIndex2 = 0;
+
+$(document).ready(function(){
+    // todo. 전역변수에 대상 변수 선언하기
+    // todo. 실행함수 불러오기 
+});

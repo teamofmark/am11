0aa2e2a56b3fc8d8f753dbe3b8da040831a6ac56
markStark <teamofmark@gmail.com>
2024-08-26 12:42:56 +0900

    움직임구현 완료

---
+++ JQuery/js/14_randomPosition.js
@@ -0,0 +1,42 @@
+var count = 0;
+var $score = null;
+var $circle = null;
+var $btnStart = null;
+var $panel = null;
+var playState = false;
+var timerID = 0;
+
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $btnStart = $("#btn_start");
+    $panel = $(".panel");
+}
+function initEvent(){
+    $btnStart.click(gameStart);
+    $circle.click(scoreCount);
+}
+function gameStart(){
+    playState = true;
+    if(playState == true){
+        gameEnd();
+        timerID = setInterval(moveCircle, 500);
+    }
+}
+function moveCircle(){
+    $circle.css({
+        // todo. 상,하,좌,우 모두 랜덤으로 박스를 벗어나지 않게 하는 수식
+        left: Math.floor(Math.random()*($panel.width() - $circle.width())),
+        top: Math.floor(Math.random()*($panel.height() - $circle.height()))
+    });
+}
+function scoreCount(){
+
+}
+function gameEnd(){
+
+}

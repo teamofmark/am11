874ca13e3a4e3d26247ee6da0fe3b66e7ddef9fc
markStark <teamofmark@gmail.com>
2024-08-28 11:38:23 +0900

    종료함수 만들기

--- JQuery/js/15_finJQuery.js
+++ JQuery/js/15_finJQuery.js
@@ -55,6 +55,7 @@ function updateFishPosition(){
     }
     console.log("5. 물고기 움직임 정상.");
 }
+// ? 물고기 위치출력 함수
 function displayFishPositionInfo(){
     // todo 경기현황판 완성시켜보기
     var info = "";
@@ -65,6 +66,34 @@ function displayFishPositionInfo(){
     $info.html(info)// todo.3 위에서 생성된 출력부를 출력.(.html()함수);
     console.log("6. 물고기 현재 위치 표시 정상.");
 }
+// ?결승선 도달 감지 함수
 function checkGoalFish(){
-
+    var winnerList = [];
+    for(var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+               index: (i),
+               position: fishCurrentPosition 
+            });
+            console.log("7. 데이터 저장 성공.");
+        }
+    }
+    if(winnerList.length>0){
+        winnerList.sort(function(a,b){
+            return b.position - a.position; //? position값 기준 내림차순 정렬
+            /*
+                오름차순 -> 진행될수록 점점 값이 커지거나(1,2,3,4...) 정순으로(ex> a,b,c,d..) 나타나는 것.
+                내림차순 -> 진행될수록 점점 값이 내려가거나(...4,3,2,1) 역순으로(ex> ...d,c,b,a) 나타나는 것.
+            */
+        });
+        endGame();
+        console.log("8. 우승 물고기 선별 완료.");
+        alert("우승!" + winnerList[0].index+"번 물고기");
+    }
+}
+function endGame(){
+    // todo. 1 setInteval Kill
+    // todo. 2 setIntervalVariable reset
+    // todo. 3 log ("9. 게임종료.") 출력.
 }

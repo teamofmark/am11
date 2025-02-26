efd18fcf9796500dce6bd0f880e547ec7e876df9
markStark <teamofmark@gmail.com>
2025-02-26 12:18:32 +0900

    다각도로 문제 해석

--- JS/step3/js/2_for.js
+++ JS/step3/js/2_for.js
@@ -13,7 +13,62 @@
     3. 참일경우 이하 구문 실행 > 4. 증감부 실행 > 5. 2단계
 
 for (var index = 0; index < 10; index++) {
-    console.log(index);
-    
+    console.log(index);  
 }
 */
+// case.1 이름을 10번 출력해보자.
+function namePrint10(){
+    var userName = "Mark";
+    for(var i = 1; i <= 10; i++){
+        document.write(i + '.' + userName + '<br>');
+    }
+}
+// case.2 이름을 1,000번 출력해보자
+function namePrint1000(){
+    var userName = "Mark";
+    for(var i = 1; i <= 1000; i++){
+        document.write(i + '.' + userName + '<br>');
+    }
+}
+// todo. 1,000번 출력중 홀수번째만 출력하게 만들어보기
+function namePrint500(){
+    var userName = "Mark";
+    for(var i = 1; i <= 1000; i+=2){
+        document.write(i + '.' + userName + '<br>');
+    }
+}
+// case.3 별(*) 10개가 찍힌 결과를 출력하자.
+function star10(){
+    var star = ''; //? star라는 빈 문자열 변수 선언
+    for(var i = 0; i < 10; i++){ //? i가 10보다 작을때 까지만(0~9) 반복 수행 가능
+        star += '*'; //? 밖에 star 변수에 반복이 실행 될 때마다 * 문자를 추가해준다.
+    } //? i가 10이 되는순간 break.
+    document.write('result = ' + star); // 반복이 끝났을 때 수행 가능.
+}
+// todo. 1 - 10까지 출력되는 반복문을 각기 다른 방식으로 5개만 짜보기
+function answer1(){
+    for (var i = 0; i < 10; i++){
+        document.write((i+1) + "<br>");
+    }
+}
+function answer2(){
+    for(var i = 10; i >=1; i--){
+        document.write((11-i)+"<br>");
+    }
+}
+function answer3(){
+    for(var i = 100; i <110; i++){
+        document.write((i-99)+"<br>");
+    }
+}
+function answer4(){
+    for(var i =1; i <= 10; i+=2){
+        document.write(i +"<br>");
+        document.write((i+1) +"<br>");
+    }
+}
+function answer5(){
+    for(var i = 1; i <=10; i++){
+        document.write(i + "<br>");
+    }
+}

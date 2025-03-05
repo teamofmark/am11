d73ddc66356e9f67eda3215281766ef2dd108740
markStark <teamofmark@gmail.com>
2025-03-05 11:44:44 +0900

    return 진입전

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -55,3 +55,30 @@ function sumParam1(){
 function sumParam(x,y){
     alert('두 수의 합은?' + (x+y));
 }
+// case. 3 매개변수의 자리가 없어도 있는 것처럼 - arguments(가변인자함수 - 배열인듯배열아닌배열같은)
+/*
+    arguments
+    - 모든 함수 안에서 사용하는 지역변수.
+    - 이를 이용하여 함수내 인수들을 참조하거나 전달된 각 인수항목을 포함 한다.
+    - array와 비슷해보이지만 length개념을 제외 하고는 array와 같지 않다.
+    - 그렇기 때문에 필요에 따라 array로 추가 변환하여 사용 할 수 있다.
+*/
+function sumAll(){
+    console.log(typeof(arguments) + ' : ' + arguments.length);
+    var willReturn = 0;
+    for(var i = 0; i<arguments.length; i++){
+        willReturn += arguments[i];
+    }
+    console.log(willReturn);
+}
+// case.4 "자신의 이름"의 나이는 "자신의 나이"세 입니다. 라는 출력문을 arguments로 만들어보기
+function showInfo(){
+    alert(arguments[0]+" 의 나이는 " + arguments[1] + "세 입니다.");
+} 
+
+/*
+    ?return (리턴)
+    * 함수 내부는 함수라는 범위 안에 갇히기 때문에, 한번 실행되면 함수 외부에서 접근 불가.
+    * 이 때 매개변수를 활용하면 함수 내부로 data를 전달 할 수 있는데, return value는 이와 반대 되는 값.
+    * 즉, 매개변수 값이 함수 외부에서 내부로 들어오는 입력 부라면, 리턴값은 함수 내부에서 처리한 결과를 함수 외부로 전달하기 위해 사용하는 출력 부.
+*/

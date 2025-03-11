ca953e6a4e11952204f83b5e49a8d99d98c9e5c0
markStark <teamofmark@gmail.com>
2025-03-11 12:23:29 +0900

    선언및초기화종료

---
+++ JQuery/js/8_randomPos.js
@@ -0,0 +1,18 @@
+var count = 0;
+var $score = null;
+var $circle = null;
+var $btnStart = null;
+var $panel = null;
+var playState = false;
+var timerID = 0;
+
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $btnStart = $("#btn_start");
+    $panel = $(".panel");
+}
